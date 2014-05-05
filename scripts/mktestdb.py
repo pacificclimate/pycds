15df52625711f42190d3c53ac53888d79cb3df62
Basil Veerman <bveerman@uvic.ca>
2014-05-05 15:00:36 -0700

    Convert to argparse, remove some defaults, add more help

--- scripts/mktestdb.py
+++ scripts/mktestdb.py
@@ -1,6 +1,6 @@
 import sys
 import logging
-from optparse import OptionParser
+from argparse import ArgumentParser
 from pkg_resources import resource_filename
 
 from sqlalchemy import create_engine
@@ -8,16 +8,16 @@ from sqlalchemy import create_engine
 from pycds.util import create_test_database, create_test_data
     
 if __name__ == '__main__':
-    parser = OptionParser()
-    parser.add_option("-d", "--dsn", dest="dsn", help="Source database DSN from which to read")
-    parser.add_option("-t", "--testdsn", dest="test_dsn", help="Destination DSN to which to write")
-    parser.set_defaults(dsn='postgresql://hiebert@monsoon.pcic/crmp?sslmode=require', testdsn='sqlite+pysqlite:////tmp/crmp.sqlite')
-    opts, args = parser.parse_args()
+    parser = ArgumentParser(description="Script to create a test CRMP database and write it to a test database (probably sqlite). DSN strings are of form:\n\tdialect+driver://username:password@host:port/database\nExamples:\n\tpostgresql://scott:tiger@localhost/mydatabase\n\tpostgresql+psycopg2://scott:tiger@localhost/mydatabase\n\tpostgresql+pg8000://scott:tiger@localhost/mydatabase")
+    parser.add_argument("-d", "--dsn", help="Source database DSN from which to read ")
+    parser.add_argument("-t", "--testdsn", help="Destination DSN to write to",
+                        default='sqlite+pysqlite:////tmp/crmp.sqlite')
+    args = parser.parse_args()
                         
     logger = logging.getLogger(__name__)
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-    read_engine = create_engine(opts.dsn)
-    write_engine = create_engine(opts.testdsn)
+    read_engine = create_engine(args.dsn)
+    write_engine = create_engine(args.testdsn)
 
     create_test_database(write_engine)
     create_test_data(write_engine)
