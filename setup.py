a9cc6ad641cdf393d46a1a4e9ff46581d39ccfc0
Basil Veerman <bveerman@uvic.ca>
2015-06-23 18:43:23 -0700

    Bump to version 0.0.19

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools.command.test import test as TestCommand
 from pkg_resources import resource_filename
 import ctypes
 import warnings
-__version__ = (0, 0, 18)
+__version__ = (0, 0, 19)
 
 class PyTest(TestCommand):
     def finalize_options(self):
