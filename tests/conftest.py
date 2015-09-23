06835d16039638cbb784cc7ff831d151cb76818b
Basil Veerman <bveerman@uvic.ca>
2015-09-23 15:21:32 -0700

    refactor db/engine creation out of test_session, fix Contacts tests using new testing data

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import sessionmaker
 import pytest
 
 import pycds
+from pycds import Network, Contact, Station, History, Variable
 
 def pytest_runtest_setup():
     logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
@@ -22,46 +23,36 @@ def blank_postgis_session():
         yield sesh
 
 @pytest.yield_fixture(scope='function')
-def test_session():
+def test_session(blank_postgis_session):
+    engine = blank_postgis_session.get_bind()
+    pycds.Base.metadata.create_all(bind=engine)
+
+    moti = Network(name='MoTIe')
+    ec = Network(name='EC')
+    wmb = Network(name='FLNROW-WMB')
+    blank_postgis_session.add_all([moti, ec, wmb])
+
+    simon = Contact(name='Simon', networks=[moti])
+    eric = Contact(name='Eric', networks=[wmb])
+    pat = Contact(name='Pat', networks=[ec])
+    blank_postgis_session.add_all([simon, eric, pat])
+
+    stations = [
+        Station(native_id='11091', network=moti, histories=[History(station_name='Brandywine')]),
+        Station(native_id='1029', network=wmb, histories=[History(station_name='FIVE MILE')]),
+        Station(native_id='2100160', network=ec, histories=[History(station_name='Beaver Creek Airport')])
+        ]
+    blank_postgis_session.add_all(stations)
+
+    variables = [Variable(name='CURRENT_AIR_TEMPERATURE1', unit='celsius', network=moti),
+                 Variable(name='precipitation', unit='mm', network=ec),
+                 Variable(name='relative_humidity', unit='percent', network=wmb)
+                 ]
+    blank_postgis_session.add_all(variables)
+
     logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO) # Let's not log all the db setup stuff...
 
-    with testing.postgresql.Postgresql() as pg:
-        engine = create_engine(pg.url())
-        sesh = sessionmaker(bind=engine)()
-
-        sesh.execute("create extension postgis")
-        res = sesh.execute("SELECT PostGIS_full_version()")
-        print res.fetchall()[0][0]
-
-        logging.getLogger('sqlalchemy.engine').setLevel(logging.ERROR)
-        pycds.Base.metadata.create_all(bind=engine)
-
-        moti = Network(name='MoTIe')
-        ec = Network(name='EC')
-        wmb = Network(name='FLNROW-WMB')
-        sesh.add_all([moti, ec, wmb])
-
-        simon = Contact(name='Simon', networks=[moti])
-        eric = Contact(name='Eric', networks=[wmb])
-        pat = Contact(name='Pat', networks=[ec])
-        sesh.add_all([simon, eric, pat])
-
-        stations = [
-            Station(native_id='11091', network=moti, histories=[History(station_name='Brandywine')]),
-            Station(native_id='1029', network=wmb, histories=[History(station_name='FIVE MILE')]),
-            Station(native_id='2100160', network=ec, histories=[History(station_name='Beaver Creek Airport')])
-            ]
-        sesh.add_all(stations)
-
-        variables = [Variable(name='CURRENT_AIR_TEMPERATURE1', unit='celsius', network=moti),
-                     Variable(name='precipitation', unit='mm', network=ec),
-                     Variable(name='relative_humidity', unit='percent', network=wmb)
-                     ]
-        sesh.add_all(variables)
-
-        logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO) # Let's not log all the db setup stuff...
-
-        yield sesh
+    yield blank_postgis_session
 
 @pytest.fixture(scope="module")
 def conn_params():
--- tests/test_contacts.py
+++ tests/test_contacts.py
@@ -3,9 +3,9 @@ from pycds import Contact, Network
 def test_have_contacts(test_session):
     q = test_session.query(Contact.name)
     rv = set([row.name for row in q])
-    assert rv == set(['Eric Meyer', 'Simon Walker'])
+    assert rv == set(['Eric', 'Simon', 'Pat'])
 
 def test_contacts_relation(test_session):
     q = test_session.query(Contact).join(Network).filter(Network.name == 'MoTIe')
     contact = q.first()
-    assert contact.name == 'Simon Walker'
+    assert contact.name == 'Simon'
