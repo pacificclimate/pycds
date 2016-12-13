8352d7011731b440e7401910c970b2ae587ee401
Rod Glover <rglover@uvic.ca>
2016-12-13 13:27:37 -0800

    Add materialized view helpers.
    These are primarily in the form of a mixin class to use when defining materialized views in SQLAlchemy.

---
+++ pycds/materialized_view_helpers.py
@@ -0,0 +1,121 @@
+from sqlalchemy.ext import compiler
+from sqlalchemy.schema import DDLElement
+from sqlalchemy import event
+from sqlalchemy import Table, Column, MetaData
+from sqlalchemy.ext.declarative import declared_attr
+from pycds.view_helpers import snake_case
+
+# Materialized view creation tools
+#
+# SQLAlchemy does not have out-of-the-box support for views or materialized views.
+# This module adds materialized view functionality.
+
+# For info on materialized view creation in SQLAlchemy see:
+#   http://www.jeffwidman.com/blog/847/using-sqlalchemy-to-create-and-manage-postgresql-materialized-views/
+#       but note this depends on flask. Essence is the same as a view.
+# For an explanation of why we use `append_column` instead of `_make_proxy`, see. This is the core stuff
+# for materialized views in SQLAlchemy, from its author:
+#   https://bitbucket.org/zzzeek/sqlalchemy/issues/3616/calling-index-on-a-materialized-view
+
+class CreateMaterializedView(DDLElement):
+    def __init__(self, name, selectable):
+        self.name = name
+        self.selectable = selectable
+
+class DropMaterializedView(DDLElement):
+    def __init__(self, name):
+        self.name = name
+
+class RefreshMaterializedView(DDLElement):
+    def __init__(self, name, concurrently=False):
+        self.name = name
+        self.concurrently = concurrently
+
+@compiler.compiles(CreateMaterializedView)
+def compile(element, compiler, **kw):
+    return "CREATE MATERIALIZED VIEW %s AS %s" % (
+        element.name,
+        compiler.sql_compiler.process(element.selectable, literal_binds=True))
+
+@compiler.compiles(DropMaterializedView)
+def compile(element, compiler, **kw):
+    return "DROP MATERIALIZED VIEW %s" % (element.name)
+
+@compiler.compiles(RefreshMaterializedView)
+def compile(element, compiler, **kw):
+    return "REFRESH MATERIALIZED VIEW %s %s" % \
+           (('CONCURRENTLY' if element.concurrently else ''), element.name)
+
+# TODO: Remove
+def create_materialized_view(name, metadata, selectable):
+    temp_metadata = MetaData()
+    t = Table(name, temp_metadata)
+    for c in selectable.c:
+        t.append_column(Column(c.name, c.type, primary_key=c.primary_key))
+
+    CreateMaterializedView(name, selectable).execute_at('after-create', metadata)
+
+    @event.listens_for(metadata, "after_create")
+    def create_indexes(target, connection, **kw):
+        for idx in t.indexes:
+            idx.create(connection)
+
+    DropMaterializedView(name).execute_at('before-drop', metadata)
+
+    return t
+
+
+class MaterializedViewMixin(object):
+    """Mixin for ORM classes that are materialized views. Defines the two key attributes of an ORM class,
+    __table__ and __mapper_args__, based on the values of class attributes __selectable__ and __primary_key__.
+
+    Usage:
+
+    class Thing(Base, MaterializedViewMixin):
+        __selectable__ = <SQLAlchemy selectable>
+        __primary_key__ = ['primary', 'key', 'columns']
+
+    __primary_key__ attribute is optional and may be omitted if __selectable__ already defines primary keys.
+    It must be defined otherwise (e.g., text selectables with anonymous columns; see tests).
+
+    """
+
+    @declared_attr
+    def __table__(cls):
+        temp_metadata = MetaData()
+        t = Table(cls.viewname(), temp_metadata)
+        for c in cls.__selectable__.c:
+            t.append_column(Column(c.name, c.type, primary_key=c.primary_key))
+
+        # Not sure if this will work, but it reproduces the setting above ...
+        @event.listens_for(cls.metadata, "after_create")
+        def create_indexes(target, connection, **kw):
+            for idx in t.indexes:
+                idx.create(connection)
+
+        return t
+        # return create_materialized_view(cls.viewname(), cls.metadata, cls.__selectable__)
+
+    @declared_attr
+    def __mapper_args__(cls):
+        # the return value should instead be merged into other __mapper_args__ declared for the class ...
+        try:
+            return {'primary_key': [cls.__table__.c[col] for col in cls.__primary_key__]}
+        except AttributeError:
+            return {}
+
+    @classmethod
+    def viewname(cls):
+        return snake_case(cls.__name__) + '_mv'
+
+    @classmethod
+    def create(cls, sesh):
+        sesh.execute(CreateMaterializedView(cls.viewname(), cls.__selectable__))
+
+    @classmethod
+    def drop(cls, sesh):
+        sesh.execute(DropMaterializedView(cls.viewname()))
+
+    @classmethod
+    def refresh(cls, sesh, concurrently=False):
+        sesh.execute(RefreshMaterializedView(cls.viewname(), concurrently))
---
+++ tests/test_materialized_view_helpers.py
@@ -0,0 +1,148 @@
+import logging, logging.config
+
+import pytest
+from pytest import fixture
+
+from sqlalchemy import Table, Column, Integer, BigInteger, Float, String, Date, DateTime, Boolean, ForeignKey, MetaData, Numeric, Interval
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import select, text, literal_column
+from sqlalchemy.sql import column
+
+from pycds.util import generic_sesh
+from pycds.materialized_view_helpers import MaterializedViewMixin
+
+# Define some simple objects (and their tables) to test view helpers against
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Thing(Base):
+    __tablename__ = 'things'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description_id = Column(Integer, ForeignKey('descriptions.id'))
+
+    def __repr__(self):
+        return '<Thing(id=%d, name=%s, desc_id=%d)>' % (self.id, self.name, self.description_id)
+
+
+class Description(Base):
+    __tablename__ = 'descriptions'
+    id = Column(Integer, primary_key=True)
+    desc = Column(String)
+
+    def __repr__(self):
+        return '<Description(id=%d, desc=%s)>' % (self.id, self.desc)
+
+
+class SimpleThing(Base, MaterializedViewMixin):
+    # __selectable__ = select([Thing]).where(Thing.id <= literal_column('3'))  # deeelightful
+
+    # less delightful but applicable to cases where we use text queries for selectable:
+    __selectable__ = text("""
+        SELECT *
+        FROM things
+        WHERE things.id <= 3
+    """).columns(Thing.id, Thing.name, Thing.description_id)
+
+    def __repr__(self):
+        return '<SimpleThing(id=%d, desc=%s)>' % (self.id, self.name)
+
+
+class ThingWithDescription(Base, MaterializedViewMixin):
+    # this works:
+    # __selectable__ = select([Thing.id, Thing.name, Description.desc])\
+    #     .select_from(Thing.__table__.join(Description.__table__, Thing.description_id == Description.id))
+
+    # this works:
+    # __selectable__ = text('''
+    #     SELECT things.id, things.name, descriptions.desc
+    #     FROM things JOIN descriptions ON (things.description_id = descriptions.id)
+    # ''').columns(Thing.id, Thing.name, Description.desc)
+
+    # so does this! which is essential for text-defined queries with new columns
+    __selectable__ = text('''
+        SELECT things.id, things.name, descriptions.desc
+        FROM things JOIN descriptions ON (things.description_id = descriptions.id)
+    ''').columns(column('id'), column('name'), column('desc'))
+    __primary_key__ = ['id']
+
+
+class ThingCount(Base, MaterializedViewMixin):
+    __selectable__ = text("""
+        SELECT d.desc as desc, count(things.id) as num
+        FROM things JOIN descriptions as d ON (things.description_id = d.id)
+        GROUP BY d.desc
+    """).columns(column('desc'), column('num'))
+    __primary_key__ = ['desc']
+
+    def __repr__(self):
+        return '<ThingWithDescription(id=%d, name=%s, desc=%s)>' % (self.id, self.name, self.desc)
+
+
+@fixture(scope="module")
+def mod_empty_test_db_session(mod_blank_postgis_session):
+    # logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+    sesh = mod_blank_postgis_session
+    engine = sesh.get_bind()
+    Base.metadata.create_all(bind=engine)
+    views = [SimpleThing, ThingWithDescription, ThingCount]
+    for view in views:
+        view.create(sesh)
+    yield sesh
+    for view in reversed(views):
+        view.drop(sesh)
+
+@fixture
+def view_test_session(mod_empty_test_db_session):
+    for sesh in generic_sesh(mod_empty_test_db_session, [Thing, Description], [
+        [
+            Description(id=1, desc='alpha'),
+            Description(id=2, desc='beta'),
+            Description(id=3, desc='gamma'),
+        ],
+        [
+            Thing(id=1, name='one', description_id=1),
+            Thing(id=2, name='two', description_id=2),
+            Thing(id=3, name='three', description_id=3),
+            Thing(id=4, name='four', description_id=2),
+            Thing(id=5, name='five', description_id=1),
+        ]
+
+    ]):
+        yield sesh
+
+def query_and_print(sesh, title, query):
+    print title
+    result = sesh.execute(query)
+    for row in result:
+        print row
+
+def test_viewname():
+    assert SimpleThing.viewname() == 'simple_thing_mv'
+
+def test_simple_view(view_test_session):
+    sesh = view_test_session
+
+    things = sesh.query(Thing)
+    assert things.count() == 5
+
+    SimpleThing.refresh(sesh)
+    view_things = sesh.query(SimpleThing)
+    assert [t.id for t in view_things.order_by(SimpleThing.id)] == [1, 2, 3]
+
+
+def test_complex_view(view_test_session):
+    sesh = view_test_session
+    ThingWithDescription.refresh(sesh)
+    things = sesh.query(ThingWithDescription)
+    assert [t.desc for t in things.order_by(ThingWithDescription.id)] \
+           == ['alpha', 'beta', 'gamma', 'beta', 'alpha']
+
+def test_counts(view_test_session):
+    sesh = view_test_session
+    ThingCount.refresh(sesh)
+    counts = sesh.query(ThingCount)
+    assert [(c.desc, c.num) for c in counts.order_by(ThingCount.desc)] == \
+           [('alpha', 2), ('beta', 2), ('gamma', 1), ]
