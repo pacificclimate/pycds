988c38d766cb380b74d406e9358e1de4325ee2c1
Rod Glover <rglover@uvic.ca>
2016-12-16 11:52:46 -0800

    Add more helper functions for loading climate baseline values

--- pycds/climate_baseline_helpers.py
+++ pycds/climate_baseline_helpers.py
@@ -1,16 +1,148 @@
-from pycds import Network
+import struct
+import datetime
+from pycds import Network, History, Variable, DerivedValue
 
-def create_PCIC_derived_variable_network(session):
-    """Create the synthetic network for derived variables"""
-    name = 'PCIC Derived Variables'
-    if session.query(Network).filter(Network.name == name).count() == 0:
-        session.add(
-            Network(
-                name=name,
-                long_name='Synthetic network for derived variables computed by PCIC',
-                # virtual='???',   # TODO: What does this mean? No existing networks define it
-                publish = False, # TODO: What does this mean?
-                # color = '#??????', # TODO: Does this need to be defined?
-            )
+pcic_climate_variable_name = 'PCIC Climate Variables'
+
+
+def get_or_create_pcic_climate_variables_network(session):
+    """Get or, if it does not exist, create the synthetic network for derived variables
+
+    Args:
+        session (...): SQLAlchemy session for accessing the database
+
+    Returns:
+        Network object for synthetic network for derived variables
+    """
+
+    network = session.query(Network).filter(Network.name == pcic_climate_variable_name).first()
+    if not network:
+        network = Network(
+            name=pcic_climate_variable_name,
+            long_name='Synthetic network for derived variables computed by PCIC',
+            # virtual='???',   # TODO: What does this mean? No existing networks define it
+            publish=False,  # TODO: What does this mean?
+            # color = '#??????', # TODO: Does this need to be defined?
         )
+        session.add(network)
+        session.flush()
+    return network
+
+
+def create_pcic_climate_baseline_variables(session):
+    """Create the derived variables for climate baseline values.
+    Create the necessary synthetic network for them if it does not already exist.
+
+    Args:
+        session (...): SQLAlchemy session for accessing the database
+
+    Returns:
+        None
+    """
+
+    network = get_or_create_pcic_climate_variables_network(session)
+
+    temp_unit = 'celsius'
+    temp_standard_name = 'air_temperature'
+    precip_unit = 'mm'
+    precip_standard_name = 'lwe_thickness_of_precipitation_amount'
+
+    variable_specs = [
+        {
+            'name': 'Tx_Climatology',
+            'unit': temp_unit,
+            'standard_name': temp_standard_name,
+            'cell_method': 't: maximum within days t: mean within months t: mean over years',
+            'description': 'Climatological mean of monthly mean of maximum daily temperature',
+            'display_name': 'Temperature Climatology (Max.)'
+        },
+        {
+            'name': 'T_mean_Climatology',
+            'unit': temp_unit,
+            'standard_name': temp_standard_name,
+            'cell_method': 't: mean within days t: mean within months t: mean over years',
+            'description': 'Climatological mean of monthly mean of mean daily temperature',
+            'display_name': 'Temperature Climatology (Mean)'
+        },
+        {
+            'name': 'Tn_Climatology',
+            'unit': temp_unit,
+            'standard_name': temp_standard_name,
+            'cell_method': 't: minimum within days t: mean within months t: mean over years',
+            'description': 'Climatological mean of monthly mean of minimum daily temperature',
+            'display_name': 'Temperature Climatology (Min.)'
+        },
+        {
+            'name': 'Precip_Climatology',
+            'unit': precip_unit,
+            'standard_name': precip_standard_name,
+            'cell_method': 't: sum within months t: mean over years',
+            'description': 'Climatological mean of monthly total precipitation',
+            'display_name': 'Precipitation Climatology'
+        },
+    ]
+
+    for vs in variable_specs:
+        variable = session.query(Variable).filter(Variable.name == vs['name']).first()
+        if not variable:
+            vs.update(short_name='{0} {1}'.format(vs['standard_name'], vs['cell_method']),
+                      network_id=network.id)
+            session.add(Variable(**vs))
+    session.flush()
+
+
+# Names and widths of fields in flat source files. Copied from Faron's R code.
+# native_id is the native_id recorded in the database for the station
+# no history record is given but can use latest for the identified station
+field_names = ['native_id', 'assay_flag', 'station_name', 'elev', 'elevation_flag', 'long', 'lat',
+               '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', 'annual']
+field_widths = [8, 1, 12, 5, 1, 12, 12, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
+
+# Format string for module `struct`
+field_format = ' '.join(['{}s'.format(fw) for fw in field_widths])
+
+
+def load_pcic_climate_baseline_values(session, var_name, source):
+    """Load baseline values into the database.
+    Create the necessary variables and synthetic network if they do not already exist.
+
+    Args:
+        session (...): SQLAlchemy session for accessing the database
+
+        var_name (str): name of climate baseline variable for which the values are to be loaded
+
+        source (iterable): an interable that returns a sequence of fixed-width formatted ASCII lines
+            (strings) containing the data to be loaded; typically a `file.readlines()`
+
+    Returns:
+        None
+    """
+
+    baseline_year = 9999  # TODO: find out what this should be
+    baseline_day = 1  # TODO: confirm
+
+    def parse_line(line):
+        field_values = struct.unpack(field_format, line.rstrip('\n'))
+        field_values = [fv.rstrip('\0 ') for fv in field_values]  # struct.unpack creates null-terminated strings
+        return dict(zip(field_names, field_values))
+
+    create_pcic_climate_baseline_variables(session)
+    variable = session.query(Variable).filter(Variable.name == var_name).first()
+
+    for line in source:
+        data = parse_line(line)
+        latest_history = session.query(History)\
+            .filter(History.station.has(native_id=data['native_id']))\
+            .order_by(History.sdate.desc())\
+            .first()
+        assert latest_history
+        session.add_all(
+            [DerivedValue(
+                time=datetime.datetime(baseline_year, month, baseline_day),
+                datum=float(data[str(month)]),
+                vars_id=variable.id,
+                history_id=latest_history.id
+            ) for month in range(1, 13)]
+        )
+
     session.flush()
--- tests/test_climate_baseline_helpers.py
+++ tests/test_climate_baseline_helpers.py
@@ -1,7 +1,14 @@
-from pytest import fixture
+import datetime
+import struct
+
+from pytest import fixture, mark
+
 import pycds
-from pycds import Network
-from pycds.climate_baseline_helpers import create_PCIC_derived_variable_network
+from pycds.util import generic_sesh
+from pycds import Network, Station, History, Variable, DerivedValue
+from pycds.climate_baseline_helpers import pcic_climate_variable_name, get_or_create_pcic_climate_variables_network, \
+    create_pcic_climate_baseline_variables, load_pcic_climate_baseline_values, field_format
+
 
 @fixture
 def empty_database_session(mod_blank_postgis_session):
@@ -10,22 +17,136 @@ def empty_database_session(mod_blank_postgis_session):
     pycds.Base.metadata.create_all(bind=engine)
     yield sesh
 
-def describe_create__PCIC__derived__variable__network():
 
-    name = 'PCIC Derived Variables'
+@fixture
+def sesh_with_climate_baseline_variables(empty_database_session):
+    sesh = empty_database_session
+    create_pcic_climate_baseline_variables(sesh)
+    yield sesh
+
+
+@fixture
+def stations():
+    return [Station(id=1, native_id='100'), Station(id=2, native_id='200'), ]
+
+
+@fixture
+def histories(stations):
+    return [History(
+        station_id=station.id,
+        station_name='Station {0}'.format(station.native_id),
+        sdate=datetime.datetime(year, 1, 1),
+        edate=datetime.datetime(year+1, 1, 1),
+    ) for station in stations for year in [2000, 2001]]
+
+
+def describe_get__or__create__pcic__climate__variables__network():
 
     def test_creates_the_expected_new_network_record(empty_database_session):
         sesh = empty_database_session
-        create_PCIC_derived_variable_network(sesh)
-        results = sesh.query(Network).filter(Network.name == name)
+        network = get_or_create_pcic_climate_variables_network(sesh)
+        results = sesh.query(Network).filter(Network.name == pcic_climate_variable_name)
         assert results.count() == 1
         result = results.first()
+        assert network.id == result.id
         assert result.publish == False
         assert 'PCIC' in result.long_name
 
-    def test_creates_at_most_one_of_them(empty_database_session):
+    def test_creates_no_more_than_one_of_them(empty_database_session):
         sesh = empty_database_session
-        create_PCIC_derived_variable_network(sesh)
-        create_PCIC_derived_variable_network(sesh)
-        results = sesh.query(Network).filter(Network.name == name)
+        get_or_create_pcic_climate_variables_network(sesh)
+        get_or_create_pcic_climate_variables_network(sesh)
+        results = sesh.query(Network).filter(Network.name == pcic_climate_variable_name)
         assert results.count() == 1
+
+def describe_create__pcic__climate__baseline__variables():
+
+    def test_causes_network_to_be_created(sesh_with_climate_baseline_variables):
+        sesh = sesh_with_climate_baseline_variables
+        results = sesh.query(Network).filter(Network.name == pcic_climate_variable_name)
+        assert results.count() == 1
+
+    @mark.parametrize('name, keyword, kwd', [
+        ('Tx_Climatology', 'maximum', 'Max.'),
+        ('T_mean_Climatology', 'mean', 'Mean'),
+        ('Tn_Climatology', 'minimum', 'Min.'),
+    ])
+    def test_creates_temperature_variables(sesh_with_climate_baseline_variables, name, keyword, kwd):
+        sesh = sesh_with_climate_baseline_variables
+        network = get_or_create_pcic_climate_variables_network(sesh)
+        result = sesh.query(Variable).filter(Variable.name == name).first()
+        assert result
+        assert (result.unit, result.standard_name, result.network_id) == \
+               (u'celsius', u'air_temperature', network.id)
+        assert result.short_name == u'air_temperature {}'.format(result.cell_method)
+        assert result.cell_method == u't: {} within days t: mean within months t: mean over years'.format(keyword)
+        assert result.description == u'Climatological mean of monthly mean of {} daily temperature'.format(keyword)
+        assert result.display_name == u'Temperature Climatology ({})'.format(kwd)
+
+    def test_creates_precip_variable(sesh_with_climate_baseline_variables):
+        sesh = sesh_with_climate_baseline_variables
+        network = get_or_create_pcic_climate_variables_network(sesh)
+        result = sesh.query(Variable).filter(Variable.name == 'Precip_Climatology').first()
+        assert (result.unit, result.standard_name, result.network_id) == \
+               (u'mm', u'lwe_thickness_of_precipitation_amount', network.id)
+        assert result.short_name == u'lwe_thickness_of_precipitation_amount {}'.format(result.cell_method)
+        assert result.cell_method == u't: sum within months t: mean over years'
+        assert result.description == u'Climatological mean of monthly total precipitation'
+        assert result.display_name == u'Precipitation Climatology'
+
+    def test_creates_no_more_than_one_of_each(empty_database_session):
+        sesh = empty_database_session
+        create_pcic_climate_baseline_variables(sesh)
+        create_pcic_climate_baseline_variables(sesh)
+        results = sesh.query(Variable).filter(Variable.name.like('%_Climatology'))
+        assert results.count() == 4
+
+
+def describe_load__pcic__climate__baseline__values():
+    def describe_with_station_and_history_records():
+
+        @fixture
+        def sesh_with_station_and_history_records(sesh_with_climate_baseline_variables, stations, histories):
+            for sesh in generic_sesh(sesh_with_climate_baseline_variables, stations + histories):
+                yield sesh
+
+        def describe_with_a_fake_source():
+            var_name = 'Tx_Climatology'  # Any valid one will do
+
+            @fixture
+            def source(stations):
+                lines = []
+                for station in stations:
+                    temps = [str(100*station.id + 2*month + 0.5) for month in range(1, 13)]
+                    temps.append('99')
+                    line = struct.pack(
+                        field_format,
+                        str(station.native_id), ' ', 'Station Name', 'elev', ' ', 'long', 'lat',
+                        *temps
+                    ).replace('\0', ' ')
+                    lines.append(line + '\n')
+                return lines
+
+            def test_loads_the_values_into_the_database(sesh_with_station_and_history_records, stations, source):
+                sesh = sesh_with_station_and_history_records
+                load_pcic_climate_baseline_values(sesh, var_name, source)
+                derived_values = sesh.query(DerivedValue)
+
+                assert derived_values.count() == 12 * len(stations)
+
+                expected_variable = sesh.query(Variable).filter_by(name=var_name).first()
+                for station in stations:
+                    station_values = derived_values.join(History).join(Station) \
+                        .filter(Station.id == station.id) \
+                        .order_by(DerivedValue.time)
+                    latest_history = sesh.query(History)\
+                        .filter(History.station.has(id=station.id))\
+                        .order_by(History.sdate.desc())\
+                        .first()
+                    for i, value in enumerate(station_values):
+                        month = i + 1
+                        assert value.time == datetime.datetime(9999, month, 1)
+                        assert value.datum == 100*station.id + 2*month + 0.5
+                        assert value.history == latest_history
+                        assert value.variable == expected_variable
+
