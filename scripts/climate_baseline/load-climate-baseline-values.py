44d3c9d2ab9e981d873edf433c2cd08999aa161f
Rod Glover <rglover@uvic.ca>
2016-12-22 14:25:34 -0800

    Remove debugging code

--- scripts/climate_baseline/load-climate-baseline-values.py
+++ scripts/climate_baseline/load-climate-baseline-values.py
@@ -8,14 +8,6 @@ from sqlalchemy.orm import sessionmaker
 
 from pycds.climate_baseline_helpers import load_pcic_climate_baseline_values
 
-# TODO: Remove
-# import struct
-# from pycds.climate_baseline_helpers import field_names, field_format
-# def parse_line(line):
-#     field_values = struct.unpack(field_format, line.rstrip('\n'))
-#     field_values = [fv.rstrip('\0 ') for fv in field_values]
-#     return dict(zip(field_names, field_values))
-
 if __name__ == '__main__':
     parser = ArgumentParser(description="""Script to load a file of climate baseline values for a single variable into a database.
 DSN strings are of form:
@@ -36,9 +28,6 @@ Examples:
     engine = create_engine(args.dsn)
     session = sessionmaker(bind=engine)()
 
-    print('variable:', args.variable)
-    print('file:', args.file)
-
     f = open(args.file)
 
     line = next(f)
@@ -46,10 +35,5 @@ Examples:
         line = next(f)  # header present; skip second header line
     else:
         f.seek(0)  # no header; reset to beginning of file
-
-    # TODO: Remove
-    # for line in islice(f, 4):
-    #     print("'{}'".format(line))
-    #     print(parse_line(line))
 
     load_pcic_climate_baseline_values(session, args.variable, f)
