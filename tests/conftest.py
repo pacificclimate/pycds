08ce61d0495f97e33d27af9c6a20f9fda5cd3ff3
Rod Glover <rglover@uvic.ca>
2017-01-03 18:01:08 -0800

    Remove unnecessary uses of id fields of objects in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -101,99 +101,90 @@ def large_test_session(blank_postgis_session):
 
 @fixture
 def network1():
-    return Network(id=1, name='Network 1')
+    return Network(name='Network 1')
 
 @fixture
 def network2():
-    return Network(id=2, name='Network 2')
+    return Network(name='Network 2')
 
 @fixture
 def station1(network1):
-    return Station(id=1, network_id=network1.id)
+    return Station(network=network1)
 
 @fixture
 def station2(network2):
-    return Station(id=2, network_id=network2.id)
+    return Station(network=network2)
 
 history_transition_date = datetime.datetime(2010, 1, 1)
 
 @fixture
 def history_stn1_hourly(station1):
-    return History(id=1, station_id=station1.id, station_name='Station 1',
+    return History(station=station1, station_name='Station 1',
                    sdate=datetime.datetime.min, edate=history_transition_date, freq='1-hourly')
 
 @fixture
 def history_stn1_12_hourly(station1):
-    return History(id=2, station_id=station1.id, station_name='Station 1',
+    return History(station=station1, station_name='Station 1',
                    sdate=datetime.datetime.min, edate=history_transition_date, freq='12-hourly')
 
 @fixture
 def history_stn1_daily(station1):
-    return History(id=3, station_id=station1.id, station_name='Station 1',
+    return History(station=station1, station_name='Station 1',
                    sdate=history_transition_date, edate=None, freq='daily')
 
 @fixture
 def history_stn2_hourly(station2):
-    return History(id=4, station_id=station2.id, station_name='Station 2',
+    return History(station=station2, station_name='Station 2',
                    sdate=datetime.datetime.min, edate=history_transition_date, freq='1-hourly')
 
 @fixture
 def var_temp_point(network1):
-    return Variable(id=10, network_id=network1.id,
-                    standard_name='air_temperature', cell_method='time: point')
+    return Variable(network=network1, standard_name='air_temperature', cell_method='time: point')
 
 @fixture
 def var_temp_point2(network2):
-    return Variable(id=11, network_id=network2.id,
-                    standard_name='air_temperature', cell_method='time: point')
+    return Variable(network=network2, standard_name='air_temperature', cell_method='time: point')
 
 @fixture
 def var_temp_max(network1):
-    return Variable(id=20, network_id=network1.id,
-                    standard_name='air_temperature', cell_method='time: maximum')
+    return Variable(network=network1, standard_name='air_temperature', cell_method='time: maximum')
 
 @fixture
 def var_temp_min(network1):
-    return Variable(id=30, network_id=network1.id,
-                    standard_name='air_temperature', cell_method='time: minimum')
+    return Variable(network=network1, standard_name='air_temperature', cell_method='time: minimum')
 
 @fixture
 def var_temp_mean(network1):
-    return Variable(id=40, network_id=network1.id,
-                    standard_name='air_temperature', cell_method='time: mean')
+    return Variable(network=network1, standard_name='air_temperature', cell_method='time: mean')
 
 @fixture
 def var_foo(network1):
-    return Variable(id=50, network_id=network1.id,
-                    standard_name='foo', cell_method='time: point')
+    return Variable(network=network1, standard_name='foo', cell_method='time: point')
 
 @fixture
 def var_precip_net1_1(network1):
-    return Variable(id=60, network_id=network1.id,
-                    standard_name='thickness_of_rainfall_amount', cell_method='time: sum')
+    return Variable(network=network1, standard_name='thickness_of_rainfall_amount', cell_method='time: sum')
 
 @fixture
 def var_precip_net1_2(network1):
-    return Variable(id=61, network_id=network1.id,
-                    standard_name='thickness_of_rainfall_amount', cell_method='time: sum')
+    return Variable(network=network1, standard_name='thickness_of_rainfall_amount', cell_method='time: sum')
 
 @fixture
 def var_precip_net2_1(network2):
-    return Variable(id=63, network_id=network2.id,
-                    standard_name='thickness_of_rainfall_amount', cell_method='time: sum')
+    return Variable(network=network2, standard_name='thickness_of_rainfall_amount', cell_method='time: sum')
 
 @fixture
 def native_flag_discard():
-    return NativeFlag(id=1, discard=True)
+    return NativeFlag(discard=True)
 
 @fixture
 def native_flag_non_discard():
-    return NativeFlag(id=2, discard=False)
+    return NativeFlag(discard=False)
 
 @fixture
 def pcic_flag_discard():
-    return PCICFlag(id=1, discard=True)
+    return PCICFlag(discard=True)
 
 @fixture
 def pcic_flag_non_discard():
-    return PCICFlag(id=2, discard=False)
+    return PCICFlag(discard=False)
