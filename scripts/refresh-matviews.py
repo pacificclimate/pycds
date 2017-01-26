a28fdc17fada2911cd0e36dd78c40995855e491d
Rod Glover <rglover@uvic.ca>
2017-01-25 16:20:41 -0800

    Add cmd-line args to select views to be refreshed

--- scripts/refresh-matviews.py
+++ scripts/refresh-matviews.py
@@ -5,13 +5,18 @@ from argparse import ArgumentParser
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from pycds.materialized_view_helpers import MaterializedViewMixin
 from pycds.weather_anomaly import \
     DailyMaxTemperature, DailyMinTemperature, \
     MonthlyAverageOfDailyMaxTemperature, MonthlyAverageOfDailyMinTemperature, \
     MonthlyTotalPrecipitation
 
+daily_views = [DailyMaxTemperature, DailyMinTemperature]
+monthly_views = [MonthlyAverageOfDailyMaxTemperature, MonthlyAverageOfDailyMinTemperature, MonthlyTotalPrecipitation]
+
 if __name__ == '__main__':
     parser = ArgumentParser(description="""Script to refresh materialized views in the PCDS/CRMP database.
+
 DSN strings are of form:
     dialect+driver://username:password@host:port/database
 Examples:
@@ -19,7 +24,9 @@ Examples:
     postgresql+psycopg2://scott:tiger@localhost/mydatabase
     postgresql+pg8000://scott:tiger@localhost/mydatabase
 """)
-    parser.add_argument("-d", "--dsn", help="Database DSN in which to create new network")
+    parser.add_argument('-d', '--dsn', help='Database DSN in which to refresh views')
+    parser.add_argument('-v', '--views', help="Views to refresh (daily | monthly | all)",
+                        choices=['daily', 'monthly', 'all'], default='all')
     args = parser.parse_args()
 
     logger = logging.getLogger(__name__)
@@ -28,11 +35,13 @@ Examples:
     engine = create_engine(args.dsn)
     session = sessionmaker(bind=engine)()
 
-    views = [
-        DailyMaxTemperature, DailyMinTemperature,
-        MonthlyAverageOfDailyMaxTemperature, MonthlyAverageOfDailyMinTemperature,
-        MonthlyTotalPrecipitation
-    ]
+    views = {
+        'daily': daily_views,
+        'monthly': monthly_views,
+        'all': daily_views + monthly_views  # order matters
+    }[args.views]
+
     for view in views:
-        logging.info("Refreshing '{}'".format(view.viewname()))
-        view.refresh(session)
+        if isinstance(view, MaterializedViewMixin):
+            logging.info("Refreshing '{}'".format(view.viewname()))
+            view.refresh(session)
