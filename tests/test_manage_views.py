635909a0094eb308629026335b52d4bce3b91832
Rod Glover <rglover@uvic.ca>
2017-01-30 16:19:56 -0800

    Test manage_views operations completely and correctly

--- tests/test_manage_views.py
+++ tests/test_manage_views.py
@@ -1,48 +1,126 @@
+import datetime
 from pytest import fixture, mark
 import testing.postgresql
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.schema import CreateSchema
 from sqlalchemy import text, inspect
 
 import pycds
-from pycds.manage_views import manage_views
+from pycds import Obs
+from pycds.manage_views import base_views, daily_views, monthly_views, manage_views
 
 
-base_view_names = ['discarded_obs_v']
-daily_view_names = ['daily_max_temperature_mv', 'daily_min_temperature_mv']
-monthly_view_names = ['monthly_average_of_daily_max_temperature_mv', 'monthly_average_of_daily_min_temperature_mv',
-                 'monthly_total_precipitation_mv']
+base_view_names = [v.viewname() for v in base_views]
+daily_view_names = [v.viewname() for v in daily_views]
+monthly_view_names = [v.viewname() for v in monthly_views]
 
 
-@fixture()
-def cleanup_db_session(session):
-    """To cause manage_views to take effect, session.commit() must be called.
-    Unfortunately, that durably modifies the database; after a commit, the normal `session` rollback doesn't have any
-    effect. So either we need to set up a database engine with 'function' scope, not 'session', or we need to clean
-    up the database each time. This fixture implements the latter."""
+@fixture(scope='function')
+def per_test_engine():
+    """Single-test database engine, so that we are starting with a clean database for each individual test.
+    Somewhat slow (computationally expensive) but simple. We need this mechanism because:
+
+    - to confirm that tables have been created, we use inspection
+      (http://docs.sqlalchemy.org/en/latest/core/reflection.html#fine-grained-reflection-with-inspector)
+    - ``inspect`` is bound to an engine, not a session
+    - any session which creates views has to commit its actions to make them visible to the engine
+    - commiting prevents the usual session rollback mechanism from working
+    - therefore we either must manually remove the tables on teardown of each session, or just use a fresh database
+    - we opt for the latter; the former proves unwieldy
+
+    Fortunately this mechanism is only necessary for testing the create operation."""
+    with testing.postgresql.Postgresql() as pg:
+        engine = create_engine(pg.url())
+        engine.execute("create extension postgis")
+        engine.execute(CreateSchema('crmp'))
+        pycds.Base.metadata.create_all(bind=engine)
+        pycds.weather_anomaly.Base.metadata.create_all(bind=engine)
+        yield engine
+
+
+@fixture
+def per_test_session(per_test_engine):
+    session = sessionmaker(bind=per_test_engine)()
+    # Default search path is `"$user", public`. Need to reset that to search crmp (for our db/orm content) and
+    # public (for postgis functions)
+    session.execute('SET search_path TO crmp, public')
+    # print('\nsearch_path', [r for r in session.execute('SHOW search_path')])
     yield session
-    for name in reversed(daily_view_names + monthly_view_names):
-        session.execute('DROP TABLE IF EXISTS crmp.{}'.format(name))
-    for name in reversed(base_view_names):
-        session.execute('DROP VIEW IF EXISTS crmp.{}'.format(name))
-    session.commit()
 
-@mark.parametrize(', operation, view, exp_matview_names', [
-    ('create', 'daily', daily_view_names),
+
+@mark.parametrize('what, exp_matview_names', [
+    ('daily', daily_view_names),
     # `create monthly` will fail if the daily views do not already exist, succeed if they do;
     # we don't bother setting up the test machinery to test that
-    ('create', 'all', daily_view_names + monthly_view_names),
+    ('all', daily_view_names + monthly_view_names),
 ])
-def test_it(engine, cleanup_db_session, operation, view, exp_matview_names):
+def test_create(per_test_engine, per_test_session, what, exp_matview_names):
 
     def check_views_and_tables(present):
         def check(expected_names, actual_names):
             for name in expected_names:
                 assert (present and (name in actual_names)) or (not present and (name not in actual_names))
 
-        check(base_view_names, inspect(engine).get_view_names(schema='crmp'))
-        check(exp_matview_names, inspect(engine).get_table_names(schema='crmp'))
+        check(base_view_names, inspect(per_test_engine).get_view_names(schema='crmp'))
+        check(exp_matview_names, inspect(per_test_engine).get_table_names(schema='crmp'))
 
     check_views_and_tables(False)
-    manage_views(cleanup_db_session, operation, view)
-    cleanup_db_session.commit()
+    manage_views(per_test_session, 'create', what)
+    per_test_session.commit()
     check_views_and_tables(True)
+
+
+@fixture
+def session_with_views(session):
+    """Test fixture for manage_views('refresh'): Session with views defined."""
+    print('\nsession_with_views: SETUP')
+    views = base_views + daily_views + monthly_views
+    for view in views:
+        view.create(session)
+    session.flush()
+    yield session
+    print('\nsession_with_views: TEARDOWN')
+    for view in reversed(views):
+        view.drop(session)
+    print('\nsession_with_views: DONE')
+
+
+@mark.parametrize('what, exp_views', [
+    ('daily', daily_views),
+    # `refresh monthly` will fail if the daily views haven't been refreshed, succeed if they do;
+    # we don't bother setting up the test machinery to test that
+    ('all', daily_views + monthly_views),
+])
+def test_refresh(session_with_views, what, exp_views,
+                 network1, station1, history_stn1_hourly, var_temp_point, var_precip_net1_1):
+
+    # initially, each view should be empty, because no data
+    for view in exp_views:
+        assert session_with_views.query(view).count() == 0
+
+    # Add observations (and all the equipment necessary therefor) so that the views will have something to chew
+    session = session_with_views
+    session.add(network1)
+    session.add(station1)
+    session.add(history_stn1_hourly)
+    session.add(var_temp_point)
+    session.add_all([
+        Obs(
+            variable=variable,
+            history=history_stn1_hourly,
+            time=datetime.datetime(2000, 1, 1, 1),
+            datum = 1.0
+        )
+        for variable in [var_temp_point, var_precip_net1_1]
+    ])
+    session.flush()
+
+    # chew
+    manage_views(session_with_views, 'refresh', what)
+    session_with_views.flush()
+
+    # now each view should contain something
+    for view in exp_views:
+        assert session_with_views.query(view).count() > 0
