2446b0e8cb60bff18ab6da79c47ce82fea40c94e
Rod Glover <rglover@uvic.ca>
2017-03-27 14:50:22 -0700

    Document function manage_views

--- pycds/manage_views.py
+++ pycds/manage_views.py
@@ -12,14 +12,20 @@ monthly_views = [MonthlyAverageOfDailyMaxTemperature, MonthlyAverageOfDailyMinTe
 logger = logging.getLogger(__name__)
 
 
-def manage_views(session, operation, which_views):
+def manage_views(session, operation, which_set):
+    """Apply specified view management operation to specified set of views.
 
-    # Order matters
+    :param session: (sqlalchemy.orm.session.Session) database session
+    :param operation: (str) operation to apply, one of 'create', 'refresh' (actually the name of any valid method
+        of a materialized view can be supplied here, but the invoking script limits it to above list).
+    :param which_set: (str) which set of views to apply the operation to, one of 'daily', 'monthly-only', 'all'
+    """
+
     views = {
         'daily': daily_views,
         'monthly-only': monthly_views,
-        'all': daily_views + monthly_views,
-    }[which_views]
+        'all': daily_views + monthly_views, # Order of view updating matters
+    }[which_set]
 
     for view in views:
         if operation == 'create' or issubclass(view, MaterializedViewMixin):
