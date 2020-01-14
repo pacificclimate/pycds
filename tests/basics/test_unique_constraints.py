f610a261230d63ce64e04e03559cb8458a374a20
Rod Glover <rglover@uvic.ca>
2020-01-14 15:40:42 -0800

    Refactor test_unique_constraints.py to use only new fixtures

--- tests/basics/test_unique_constraints.py
+++ tests/basics/test_unique_constraints.py
@@ -1,32 +1,33 @@
 from datetime import datetime
 
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from pycds.util import create_test_database, create_test_data
 from pycds import Obs, History, Variable, Network, NativeFlag
 
 import pytest
 
-def test_obs_raw_unique(test_session):
+
+def test_obs_raw_unique(tfs_pycds_sesh_with_small_data):
+    sesh = tfs_pycds_sesh_with_small_data
     # Find any variable and history to link to
-    hist = test_session.query(History).first()
-    var = test_session.query(Variable).first()
+    hist = sesh.query(History).first()
+    var = sesh.query(Variable).first()
     time = datetime(1980, 1, 1)
     # Add duplicate observations
     for _ in range(2):
         o = Obs(time=time, datum=0, history=hist, variable=var)
-        test_session.add(o)
+        sesh.add(o)
 
     with pytest.raises(IntegrityError):
-        test_session.commit()
+        sesh.commit()
 
-def test_native_flag_unique(test_session):
+
+def test_native_flag_unique(tfs_pycds_sesh_with_small_data):
+    sesh = tfs_pycds_sesh_with_small_data
     # Pick a network, any network
-    net = test_session.query(Network).first()
+    net = sesh.query(Network).first()
     for _ in range(2):
         flag = NativeFlag(network=net, value='foo')
-        test_session.add(flag)
+        sesh.add(flag)
 
     with pytest.raises(IntegrityError):
-        test_session.commit()
+        sesh.flush()
