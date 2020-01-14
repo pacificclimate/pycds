dab6df5b20565305c667df292241b95f57b460d1
Rod Glover <rglover@uvic.ca>
2020-01-14 12:06:59 -0800

    Refactor tests of materialized view helpers

--- tests/view_helpers/conftest.py
+++ tests/view_helpers/conftest.py
@@ -2,7 +2,9 @@ from pytest import fixture
 from sqlalchemy.orm import sessionmaker
 from pycds.util import generic_sesh
 from .content import \
-    ContentBase, Thing, Description, SimpleThingView, ThingWithDescriptionView, ThingCountView
+    ContentBase, Thing, Description, \
+    SimpleThingView, ThingWithDescriptionView, ThingCountView, \
+    SimpleThingMatview, ThingWithDescriptionMatview, ThingCountMatview
 
 
 @fixture  # TODO: scope?
@@ -21,30 +23,39 @@ def tst_orm_sesh(tst_orm_engine, set_search_path):
     sesh.close()
 
 
-@fixture  # TODO: scope?
-def tst_orm_content_sesh(tst_orm_sesh):
-    for sesh in generic_sesh(tst_orm_sesh, [
-        Description(id=1, desc='alpha'),
-        Description(id=2, desc='beta'),
-        Description(id=3, desc='gamma'),
-        Thing(id=1, name='one', description_id=1),
-        Thing(id=2, name='two', description_id=2),
-        Thing(id=3, name='three', description_id=3),
-        Thing(id=4, name='four', description_id=2),
-        Thing(id=5, name='five', description_id=1),
-    ]):
-        yield sesh
+content = [
+    Description(id=1, desc='alpha'),
+    Description(id=2, desc='beta'),
+    Description(id=3, desc='gamma'),
+    Thing(id=1, name='one', description_id=1),
+    Thing(id=2, name='two', description_id=2),
+    Thing(id=3, name='three', description_id=3),
+    Thing(id=4, name='four', description_id=2),
+    Thing(id=5, name='five', description_id=1),
+]
 
 
 @fixture  # TODO: scope?
-def view_sesh(tst_orm_content_sesh):
-    sesh = tst_orm_content_sesh
+def view_sesh(tst_orm_sesh):
+    sesh = tst_orm_sesh
     views = [SimpleThingView, ThingWithDescriptionView, ThingCountView]
     for view in views:
         view.create(sesh)
-    yield sesh
+    for s in generic_sesh(tst_orm_sesh, content):
+        yield s
     for view in reversed(views):
         view.drop(sesh)
 
 
-
+@fixture  # TODO: scope?
+def matview_sesh(tst_orm_sesh):
+    sesh = tst_orm_sesh
+    views = [SimpleThingMatview, ThingWithDescriptionMatview, ThingCountMatview]
+    # Matviews must be created before content is added in order to test
+    # refreshed functionality.
+    for view in views:
+        view.create(sesh)
+    for s in generic_sesh(tst_orm_sesh, content):
+        yield s
+    for view in reversed(views):
+        view.drop(sesh)
--- tests/view_helpers/content.py
+++ tests/view_helpers/content.py
@@ -1,5 +1,6 @@
 """
-Define some simple ORM objects (and their tables) to test view helpers against.
+Define some simple ORM objects (and their tables) to test view and matview
+helpers against.
 To separate view creation from creation of non-view (content) tables, they
 are given different ORM declarative bases.
 """
@@ -10,11 +11,14 @@ from sqlalchemy.sql import select, text, literal_column
 from sqlalchemy.sql import column
 
 from pycds.view_helpers import ViewMixin
+from pycds.materialized_view_helpers import MaterializedViewMixin
 
 
 ContentBase = declarative_base()
 ViewBase = declarative_base()
 
+
+# Tables
 
 class Thing(ContentBase):
     __tablename__ = 'things'
@@ -34,39 +38,76 @@ class Description(ContentBase):
     def __repr__(self):
         return '<Description(id={}, desc={})>'.format(self.id, self.desc)
 
+# Selectables for defining views and matviews
 
-class SimpleThing(ViewBase, ViewMixin):
-    # __selectable__ = select([Thing]).where(Thing.id <= literal_column('3'))  # deeelightful
+simple_thing_sqla_selectable = \
+    select([Thing]).where(Thing.id <= literal_column('3'))
 
-    # less delightful but applicable to cases where we use text queries for selectable:
-    __selectable__ = text("""
-        SELECT *
-        FROM things
-        WHERE things.id <= 3
-    """).columns(Thing.id, Thing.name, Thing.description_id)
+# less delightful but applicable to cases where we use text queries for
+# selectable:
+simple_thing_text_selectable = text('''
+    SELECT *
+    FROM things
+    WHERE things.id <= 3
+''').columns(Thing.id, Thing.name, Thing.description_id)
+
+
+thing_with_description_text_selectable = text('''
+    SELECT things.id, things.name, descriptions.desc
+    FROM things JOIN descriptions ON (things.description_id = descriptions.id)
+''').columns(column('id'), column('name'), column('desc'))
+
+thing_count_text_selectable = text('''
+    SELECT d.desc as desc, count(things.id) as num
+    FROM things JOIN descriptions as d ON (things.description_id = d.id)
+    GROUP BY d.desc
+''').columns(column('desc'), column('num'))
+
+
+# Views
+
+class SimpleThingView(ViewBase, ViewMixin):
+    __selectable__ = simple_thing_text_selectable
 
     def __repr__(self):
-        return '<SimpleThing(id={}, desc={})>'.format(self.id, self.name)
+        return '<SimpleThingView(id={}, desc={})>'.format(self.id, self.name)
 
 
-class ThingWithDescription(ViewBase, ViewMixin):
-    __selectable__ = text('''
-        SELECT things.id, things.name, descriptions.desc
-        FROM things JOIN descriptions ON (things.description_id = descriptions.id)
-    ''').columns(column('id'), column('name'), column('desc'))
+class ThingWithDescriptionView(ViewBase, ViewMixin):
+    __selectable__ = thing_with_description_text_selectable
     __primary_key__ = ['id']
 
 
-class ThingCount(ViewBase, ViewMixin):
-    __selectable__ = text("""
-        SELECT d.desc as desc, count(things.id) as num
-        FROM things JOIN descriptions as d ON (things.description_id = d.id)
-        GROUP BY d.desc
-    """).columns(column('desc'), column('num'))
+class ThingCountView(ViewBase, ViewMixin):
+    __selectable__ = thing_count_text_selectable
     __primary_key__ = ['desc']
 
     def __repr__(self):
-        return '<ThingWithDescription(id={}, name={}, desc={})>'.format(self.id, self.name, self.desc)
+        return '<ThingWithDescriptionView(id={}, name={}, desc={})>'.format(self.id, self.name, self.desc)
+
+
+# Materialized views
+
+class SimpleThingMatview(ViewBase, MaterializedViewMixin):
+    __selectable__ = simple_thing_text_selectable
+
+    def __repr__(self):
+        return '<SimpleThingMatview(id={}, desc={})>'.format(self.id, self.name)
+
+
+class ThingWithDescriptionMatview(ViewBase, MaterializedViewMixin):
+    __selectable__ = thing_with_description_text_selectable
+    __primary_key__ = ['id']
+
+
+class ThingCountMatview(ViewBase, MaterializedViewMixin):
+    __selectable__ = thing_count_text_selectable
+    __primary_key__ = ['desc']
+
+    def __repr__(self):
+        return '<ThingWithDescriptionMatview(id={}, name={}, desc={})>'.format(self.id, self.name, self.desc)
+
+
 
 
 
