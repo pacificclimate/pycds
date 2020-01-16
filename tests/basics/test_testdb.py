514f11f760fa68f0e1544c2bb92fa6833a853c57
Rod Glover <rglover@uvic.ca>
2020-01-16 10:54:45 -0800

    DRY up a test

--- tests/basics/test_testdb.py
+++ tests/basics/test_testdb.py
@@ -1,15 +1,9 @@
-from ..helpers import insert_test_data, insert_crmp_data
+from ..helpers import get_items_in_schema, insert_test_data, insert_crmp_data
 from pycds import Contact, History, Obs
 
 
-def test_reflect_tables_into_session(schema_name, pycds_sesh):
-    res = pycds_sesh.execute(f'''
-        SELECT table_name 
-        FROM information_schema.tables 
-        WHERE table_schema = '{schema_name}';
-    ''')
-
-    assert {x[0] for x in res.fetchall()} >= {
+def test_schema_content(pycds_sesh):
+   assert get_items_in_schema(pycds_sesh, 'tables') >= {
         'meta_sensor', 'meta_contact', 'climo_obs_count_mv',
         'obs_count_per_month_history_mv',
         'vars_per_history_mv', 'meta_history',
