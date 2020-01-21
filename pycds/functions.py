94f8d7913a054bec69645a65a4aa00ca96131241
Rod Glover <rglover@uvic.ca>
2020-01-20 16:22:37 -0800

    Fix problems in functions

--- pycds/functions.py
+++ pycds/functions.py
@@ -27,6 +27,21 @@ at least the following options:
    and matviews. This is currently the preferred approach.
 
 """
+__all__ = [
+    'closest_stns_within_threshold',
+    'daily_ts',
+    'daysinmonth',
+    'do_query_one_station',
+    'effective_day',
+    'getstationvariabletable',
+    'lastdateofmonth',
+    'monthly_ts',
+    'query_one_station',
+    'query_one_station_climo',
+    'season',
+    'updatesdateedate',
+]
+
 
 from sqlalchemy.schema import DDL
 from pycds import get_schema_name
@@ -57,13 +72,15 @@ def closest_stns_within_threshold(schema=get_schema_name()):
         mystr TEXT;
     BEGIN
         mystr = 'WITH stns_in_thresh AS (
-        SELECT history_id, lat, lon, Geography(ST_Transform(the_geom,4326)) as p_existing, Geography(ST_SetSRID(ST_MakePoint('|| X ||','|| Y ||'),4326)) as p_new
-        FROM meta_history
+        SELECT history_id, lat, lon, 
+            Geography(ST_Transform(the_geom,4326)) as p_existing, 
+            Geography(ST_SetSRID(ST_MakePoint('|| X ||','|| Y ||'),4326)) as p_new
+        FROM {schema}.meta_history
         WHERE the_geom && ST_Buffer(Geography(ST_SetSRID(ST_MakePoint('|| X || ','|| Y ||'),4326)),'|| thres ||')
-    )
-    SELECT history_id, lat, lon, ST_Distance(p_existing,p_new) as dist
-    FROM stns_in_thresh
-    ORDER BY dist';
+        )
+        SELECT history_id, lat, lon, ST_Distance(p_existing,p_new) as dist
+        FROM stns_in_thresh
+        ORDER BY dist';
         RETURN QUERY EXECUTE mystr;
     END;
     $BODY$
@@ -74,6 +91,10 @@ def closest_stns_within_threshold(schema=get_schema_name()):
 
 
 def daily_ts(schema=get_schema_name()):
+    """Note: This function has an error in it: The table `obs_raw` has no
+    column `station_id`. It seems likely that the statement executed should
+    contain a join to `meta_history`.
+    """
     return DDL(f'''
     CREATE OR REPLACE FUNCTION {schema}.daily_ts(
         IN station_id integer,
@@ -87,11 +108,11 @@ def daily_ts(schema=get_schema_name()):
     $BODY$
     DECLARE
     BEGIN
-        RAISE DEBUG 'Running daily_ts "%" "%" "%"', station_id, vars_id, percent_obs;
+        RAISE DEBUG 'Running daily_ts "%%" "%%" "%%"', station_id, vars_id, percent_obs;
         FOR daily_time, daily_mean, daily_count IN EXECUTE
-            E'SELECT date_trunc(\'day\', obs_time) as obs_time_trunc, avg(datum) as obs_datum, count(datum) as obs_count FROM obs_raw WHERE station_id = ' || station_id || ' AND vars_id = ' || vars_id || ' GROUP BY obs_time_trunc ORDER BY obs_time_trunc'
+            'SELECT date_trunc(''day'', obs_time) as obs_time_trunc, avg(datum) as obs_datum, count(datum) as obs_count FROM {schema}.obs_raw WHERE station_id = ' || station_id || ' AND vars_id = ' || vars_id || ' GROUP BY obs_time_trunc ORDER BY obs_time_trunc'
         LOOP
-            RAISE DEBUG 'In loop, Row: "%" "%" "%"', daily_time, daily_mean, daily_count;
+            RAISE DEBUG 'In loop, Row: "%%" "%%" "%%"', daily_time, daily_mean, daily_count;
             percent_obs_available := daily_count / 24.0;
                 IF percent_obs_available >= percent_obs THEN
                RAISE DEBUG 'Conditional is TRUE';
@@ -133,7 +154,7 @@ def do_query_one_station(schema=get_schema_name()):
         query text;
         result refcursor := 'result';
     BEGIN
-        query := query_one_station(station_id);
+        query := {schema}.query_one_station(station_id);
         OPEN result NO SCROLL FOR EXECUTE query;
         RETURN result;
     END;
@@ -171,25 +192,25 @@ def effective_day(schema=get_schema_name()):
         freq character varying DEFAULT ''::character varying)
       RETURNS timestamp without time zone AS
     $BODY$
-            DECLARE
-              offs INTERVAL; -- 'offset' is a reserved word
-              hour INTEGER;
-            BEGIN
-              offs := '0'::INTERVAL;
-              IF extremum = 'max' THEN
-                  hour := date_part('hour', obs_time);
-                  IF freq = '12-hourly' AND hour >= 12 THEN
-                    offs = '1 day'::INTERVAL;
-                  END IF;
-              ELSE
-                offs = '0'::INTERVAL;
-              END IF;
-              RETURN date_trunc('day', obs_time) + offs;
-            END;
-            $BODY$
+    DECLARE
+      offs INTERVAL; -- 'offset' is a reserved word
+      hour INTEGER;
+    BEGIN
+      offs := '0'::INTERVAL;
+      IF extremum = 'max' THEN
+          hour := date_part('hour', obs_time);
+          IF freq = '12-hourly' AND hour >= 12 THEN
+            offs = '1 day'::INTERVAL;
+          END IF;
+      ELSE
+        offs = '0'::INTERVAL;
+      END IF;
+      RETURN date_trunc('day', obs_time) + offs;
+    END;
+    $BODY$
       LANGUAGE plpgsql VOLATILE
       COST 100;
-      ''')
+    ''')
 
 
 # Returns the text of a SELECT statement for a table containing the values
@@ -215,16 +236,16 @@ def getstationvariabletable(schema=get_schema_name()):
         climo boolean)
       RETURNS text AS
     $BODY$
-        query = "SELECT vars_id, net_var_name FROM meta_vars NATURAL JOIN meta_station WHERE cell_method " + ("" if climo else "!") + "~ '(within|over)' AND station_id = " + str(station_id) + ' ORDER BY net_var_name'
+        query = "SELECT vars_id, net_var_name FROM {schema}.meta_vars NATURAL JOIN {schema}.meta_station WHERE cell_method " + ("" if climo else "!") + "~ '(within|over)' AND station_id = " + str(station_id) + ' ORDER BY net_var_name'
         data = plpy.execute(query)
         bits = [ 'obs_time' ] + [ ("MAX(CASE WHEN vars_id=" + str(x['vars_id']) + " THEN datum END) as " + x['net_var_name']) for x in data ]
         vars_ids = [ str(x['vars_id']) for x in data ]
     
-        hid_query = "SELECT history_id FROM meta_history WHERE station_id=" + str(station_id)
+        hid_query = "SELECT history_id FROM {schema}.meta_history WHERE station_id=" + str(station_id)
         hid_data = plpy.execute(hid_query)
         hid_clauses = [ "history_id = " + str(x['history_id']) for x in hid_data ]
     
-        return "SELECT "+ ",".join(bits) + " from obs_raw WHERE (" + " OR ".join(hid_clauses) + ") AND vars_id IN (" + ",".join(vars_ids) + ") GROUP BY obs_time ORDER BY obs_time"
+        return "SELECT "+ ",".join(bits) + " from {schema}.obs_raw WHERE (" + " OR ".join(hid_clauses) + ") AND vars_id IN (" + ",".join(vars_ids) + ") GROUP BY obs_time ORDER BY obs_time"
     $BODY$
       LANGUAGE plpythonu VOLATILE
       COST 100;
@@ -259,20 +280,19 @@ def monthly_ts(schema=get_schema_name()):
     DECLARE
         the_month date;
     BEGIN
-        RAISE DEBUG 'Running monthly_ts "%" "%" "%"', station_id, vars_id, percent_obs;
+        RAISE DEBUG 'Running monthly_ts "%%" "%%" "%%"', station_id, vars_id, percent_obs;
         FOR monthly_time, monthly_mean, monthly_count IN EXECUTE
-            E'SELECT date_trunc(\'month\', obs_time) as obs_time_trunc, avg(datum) as obs_datum, count(datum) as obs_count FROM obs_raw WHERE station_id = ' || station_id || ' AND vars_id = ' || vars_id || ' GROUP BY obs_time_trunc ORDER BY obs_time_trunc'
+            'SELECT date_trunc(''month'', obs_time) as obs_time_trunc, avg(datum) as obs_datum, count(datum) as obs_count FROM {schema}.obs_raw WHERE station_id = ' || station_id || ' AND vars_id = ' || vars_id || ' GROUP BY obs_time_trunc ORDER BY obs_time_trunc'
         LOOP
-            RAISE DEBUG 'In loop, Row: "%" "%" "%"', monthly_time, monthly_mean, monthly_count;
+            RAISE DEBUG 'In loop, Row: "%%" "%%" "%%"', monthly_time, monthly_mean, monthly_count;
             the_month := CAST(monthly_time AS date);
-            percent_obs_available := monthly_count / (DaysInMonth(the_month));
+            percent_obs_available := monthly_count / ({schema}.DaysInMonth(the_month));
                 IF percent_obs_available >= percent_obs THEN
                RAISE DEBUG 'Conditional is TRUE';
                RETURN NEXT;
             END IF;
         END LOOP;
         RETURN;
-    
     END;
     $BODY$
       LANGUAGE plpgsql VOLATILE
@@ -290,7 +310,7 @@ def query_one_station(schema=get_schema_name()):
     CREATE OR REPLACE FUNCTION {schema}.query_one_station(station_id integer)
       RETURNS text AS
     $BODY$
-        stn_query = "SELECT * FROM getStationVariableTable(" + str(station_id) + ", false)"
+        stn_query = "SELECT * FROM {schema}.getStationVariableTable(" + str(station_id) + ", false)"
         data = plpy.execute(stn_query)
         #plpy.warning(data)
         return data[0]['getstationvariabletable']
@@ -309,7 +329,7 @@ def query_one_station_climo(schema=get_schema_name()):
     CREATE OR REPLACE FUNCTION {schema}.query_one_station_climo(station_id integer)
       RETURNS text AS
     $BODY$
-        stn_query = "SELECT * FROM getStationVariableTable(" + str(station_id) + ", true)"
+        stn_query = "SELECT * FROM {schema}.getStationVariableTable(" + str(station_id) + ", true)"
         data = plpy.execute(stn_query)
         #plpy.warning(data)
         return data[0]['getstationvariabletable']
@@ -351,14 +371,15 @@ def updatesdateedate(schema=get_schema_name()):
         min_date timestamp without time zone;
         max_date timestamp without time zone;
     BEGIN
-        FOR sid IN SELECT DISTINCT station_id FROM meta_history
+        FOR sid IN SELECT DISTINCT station_id FROM {schema}.meta_history
         LOOP
             SELECT min(obs_time), max(obs_time) INTO min_date, max_date
-                   FROM obs_raw NATURAL JOIN meta_history WHERE station_id = sid;
+            FROM {schema}.obs_raw NATURAL JOIN {schema}.meta_history 
+            WHERE station_id = sid;
     
-            UPDATE meta_station
-                   SET (min_obs_time, max_obs_time) = (min_date, max_date)
-                   WHERE station_id = sid;
+            UPDATE {schema}.meta_station
+            SET (min_obs_time, max_obs_time) = (min_date, max_date)
+            WHERE station_id = sid;
     END LOOP;
     RETURN;
     END;
@@ -366,3 +387,4 @@ def updatesdateedate(schema=get_schema_name()):
       LANGUAGE plpgsql VOLATILE
       COST 100;
     ''')
+
