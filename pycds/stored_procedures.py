e0375ae26626c8c6d359dc616a4198b752c81940
Rod Glover <rglover@uvic.ca>
2020-01-30 12:13:48 -0800

    Substitute tuple for 2 args in SP

--- pycds/stored_procedures.py
+++ pycds/stored_procedures.py
@@ -5,42 +5,40 @@ from alembic.operations import Operations, MigrateOperation
 class CreateStoredProcedureOp(MigrateOperation):
     """Create a FUNCTION."""
 
-    def __init__(self, name, sqltext, schema=None):
-        self.name = name
-        self.sqltext = sqltext
+    def __init__(self, target, schema=None):
+        self.name, self.sqltext = target
         self.schema = schema
 
     @classmethod
-    def create_stored_procedure(cls, operations, name, sqltext, **kw):
+    def create_stored_procedure(cls, operations, target, **kw):
         """Issue a "CREATE SEQUENCE" instruction."""
 
-        op = CreateStoredProcedureOp(name, sqltext, **kw)
+        op = CreateStoredProcedureOp(target, **kw)
         return operations.invoke(op)
 
     def reverse(self):
         # only needed to support autogenerate
-        return DropStoredProcedureOp(self.name, self.sqltext, schema=self.schema)
+        return DropStoredProcedureOp((self.name, self.sqltext), schema=self.schema)
 
 
 @Operations.register_operation("drop_stored_procedure")
 class DropStoredProcedureOp(MigrateOperation):
     """Drop a FUNCTION."""
 
-    def __init__(self, name, sqltext, schema=None):
-        self.name = name
-        self.sqltext = sqltext
+    def __init__(self, target, schema=None):
+        self.name, self.sqltext = target
         self.schema = schema
 
     @classmethod
-    def drop_stored_procedure(cls, operations, name, sqltext, **kw):
+    def drop_stored_procedure(cls, operations, target, **kw):
         """Issue a "DROP SEQUENCE" instruction."""
 
-        op = DropStoredProcedureOp(name, sqltext, **kw)
+        op = DropStoredProcedureOp(target, **kw)
         return operations.invoke(op)
 
     def reverse(self):
         # only needed to support autogenerate
-        return CreateStoredProcedureOp(self.name, self.sqltext, schema=self.schema)
+        return CreateStoredProcedureOp((self.name, self.sqltext), schema=self.schema)
 
 
 @Operations.implementation_for(CreateStoredProcedureOp)
