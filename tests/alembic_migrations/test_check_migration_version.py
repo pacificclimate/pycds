f4412e2a2cdf29ef70073956b5bab1d50e5ef971
Rod Glover <rglover@uvic.ca>
2020-02-12 16:47:24 -0800

    Add comments

--- tests/alembic_migrations/test_check_migration_version.py
+++ tests/alembic_migrations/test_check_migration_version.py
@@ -17,9 +17,14 @@ def test_check_migration_version(
     engine, script = prepare_schema_from_migrations(
         uri_left, alembic_config_left, db_setup=db_setup
     )
+
+    # Test against the most up to date migration in the database;
+    # this should pass, i.e., not raise an exception.
     check_migration_version(engine)
 
+    # Back off to the previous migration.
     command.downgrade(alembic_config_left, '-1')
 
+    # Now the checker should raise an exception.
     with pytest.raises(ValueError):
         check_migration_version(engine)
