fb9d37187b23d3025580b2f703c646dfb49018e0
rod-glover <rod.glover.ca@gmail.com>
2021-02-10 12:51:40 -0800

    Fix matview definition

--- pycds/orm/native_matviews/version_7a3b247c577b.py
+++ pycds/orm/native_matviews/version_7a3b247c577b.py
@@ -1,22 +1,10 @@
-"""Materialized views
-
-Define materialized views as proper SQL native matviews in the database.
-
-SQLAlchemy does not directly support matviews. A view can be represented simply
-as a table in the ORM, but this does not permit its definition to be part of
-the ORM, or to be maintained and migrated.
-
-This module defines matviews in the ORM as SQL views in the database, using
-a SQLAlchemy compiler extension provided by `./materialized_view_helpers`.
-See that module for more information. In particular, see the note about using
-a separate declarative base for matviews; here it is `Base`.
-"""
-
-from sqlalchemy import MetaData, Index, Column, Integer, ForeignKey
+from sqlalchemy import MetaData, Index, Column, Integer
 from sqlalchemy.orm import Query
 from sqlalchemy.ext.declarative import declarative_base
 from pycds.util import get_schema_name
-from pycds.alembic.extensions.replaceable_objects import ReplaceableNativeMatview
+from pycds.alembic.extensions.replaceable_objects import (
+    ReplaceableNativeMatview,
+)
 from pycds.orm.tables import Obs
 
 # TODO: Factor Base one level up, common to all versions
@@ -41,18 +29,16 @@ class VarsPerHistory(Base, ReplaceableNativeMatview):
     SELECT DISTINCT obs_raw.history_id, obs_raw.vars_id
     FROM obs_raw;
     """
+
     __tablename__ = "vars_per_history_mv"
 
-    history_id = Column(
-        Integer, ForeignKey("meta_history.history_id"), primary_key=True
-    )
-    vars_id = Column(Integer, ForeignKey("meta_vars.vars_id"), primary_key=True)
+    history_id = Column(Integer, primary_key=True)
+    vars_id = Column(Integer, primary_key=True)
 
     __selectable__ = (
-        Query([
-            Obs.history_id.label("history_id"),
-            Obs.vars_id.label("vars_id")
-        ]).distinct()
+        Query(
+            [Obs.history_id.label("history_id"), Obs.vars_id.label("vars_id")]
+        ).distinct()
     ).selectable
 
 
