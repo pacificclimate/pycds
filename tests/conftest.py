a083b8f30015773a0ef4e6a773d427c076deeb33
rod-glover <rod.glover.ca@gmail.com>
2021-02-11 12:36:18 -0800

    Reduce sqlalchemy logging in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,7 +15,7 @@ def pytest_runtest_setup():
     logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
     logging.getLogger('tests').setLevel(logging.DEBUG)
     logging.getLogger('alembic').setLevel(logging.DEBUG)
-    logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+    # logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
     # logging.getLogger("sqlalchemy.pool").setLevel(logging.DEBUG)
 
 @fixture(scope='session')
