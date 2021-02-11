64cc0f2d6914459779d4aaee09122b8b0502c015
rod-glover <rod.glover.ca@gmail.com>
2021-02-11 11:25:29 -0800

    Add replaceable stored procedure classes

--- pycds/alembic/extensions/replaceable_objects.py
+++ pycds/alembic/extensions/replaceable_objects.py
@@ -1,20 +1,25 @@
 """
-This module defines base classes for defining replaceable objects. SQLAlchemy
-documentation often refers to classes intended to supply extra features in
-ORM classes as "mixins", but we prefer to reserve that terminology for classes
+This module defines base classes for defining replaceable objects.
+
+Note: SQLAlchemy documentation often refers to classes that supply extra
+features in ORM classes as "mixins", but we reserve that terminology for classes
 that actually affect the mapping proper, not just adding an API for other use.
 
 TBD: Add references to documentation.
 """
 
-from pycds.util import snake_case
+from pycds.util import snake_case, ddl_escape
 from pycds.sqlalchemy.ddl_extensions import (
     CreateView,
     DropView,
     CreateMaterializedView,
     DropMaterializedView,
     RefreshMaterializedView,
+    CreateStoredProcedure,
+    DropStoredProcedure,
 )
+
+# Replaceable objects represented by ORM classes
 
 
 class ReplaceableOrmClass:
@@ -39,12 +44,14 @@ class ReplaceableOrmClass:
 
     @classmethod
     def qualified_name(cls):
-        prefix = '' if cls.metadata.schema is None \
-            else cls.metadata.schema + '.'
+        prefix = (
+            "" if cls.metadata.schema is None else cls.metadata.schema + "."
+        )
         return prefix + cls.base_name()
 
 
 # Views
+
 
 class ReplaceableView(ReplaceableOrmClass):
     """
@@ -75,10 +82,11 @@ class ReplaceableView(ReplaceableOrmClass):
         try:
             return cls.__tablename__
         except AttributeError:
-            return snake_case(cls.__name__) + '_v'
+            return snake_case(cls.__name__) + "_v"
 
 
 # Native materialized views
+
 
 class ReplaceableNativeMatview(ReplaceableOrmClass):
     """
@@ -92,31 +100,26 @@ class ReplaceableNativeMatview(ReplaceableOrmClass):
 
     @classmethod
     def create(cls):
-        return CreateMaterializedView(
-            cls.qualified_name(), cls.__selectable__
-        )
+        return CreateMaterializedView(cls.qualified_name(), cls.__selectable__)
 
     @classmethod
     def drop(cls):
-        return DropMaterializedView(
-            cls.qualified_name(), cls.__selectable__
-        )
+        return DropMaterializedView(cls.qualified_name(), cls.__selectable__)
 
     @classmethod
     def refresh(cls):
-        return RefreshMaterializedView(
-            cls.qualified_name(), cls.__selectable__
-        )
+        return RefreshMaterializedView(cls.qualified_name(), cls.__selectable__)
 
     @classmethod
     def base_name(cls):
         try:
             return cls.__tablename__
         except AttributeError:
-            return snake_case(cls.__name__) + '_mv'
+            return snake_case(cls.__name__) + "_mv"
 
 
 # Manual materialized views
+
 
 class ReplaceableManualMatview(ReplaceableOrmClass):
     """
@@ -151,4 +154,48 @@ class ReplaceableManualMatview(ReplaceableOrmClass):
         try:
             return cls.__tablename__
         except AttributeError:
-            return snake_case(cls.__name__) + '_mv'
+            return snake_case(cls.__name__) + "_mv"
+
+
+# Replaceable objects represented by objects (not classes)
+
+
+class ReplaceableObject:
+    """A very simple way to represent a named object."""
+
+    def __init__(self, identifier, definition, schema=None):
+        self.identifier = identifier
+        self.definition = definition
+        self.schema = schema
+
+    def qualified_name(self):
+        prefix = "" if self.schema is None else f"{self.schema}."
+        return f"{prefix}{self.identifier}"
+
+    def create(self):
+        raise NotImplementedError()
+
+    def drop(self):
+        raise NotImplementedError()
+
+
+# Replaceable stored procedure
+
+
+class ReplaceableStoredProcedure(ReplaceableObject):
+    def __init__(self, identifier, definition, schema=None, escape=True):
+        # DDL statements substitute special % expressions, and literal '%' must
+        # be escaped as '%%'. This is the default behaviour of this class. This
+        # is of particular concern for stored procedures as some of the
+        # languages themselves use %-substitution.
+        super().__init__(identifier, definition, schema)
+        self.escape = escape
+
+    def create(self):
+        definition = (
+            ddl_escape(self.definition) if self.escape else self.definition
+        )
+        return CreateStoredProcedure(self.qualified_name(), definition)
+
+    def drop(self):
+        return DropStoredProcedure(self.qualified_name())
