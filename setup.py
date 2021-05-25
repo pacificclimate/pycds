5dcf5f456ac37b8173584ff8fe255fa2392ec82c
rod-glover <rod.glover.ca@gmail.com>
2021-05-25 14:21:34 -0700

    Blacken setup.py

--- setup.py
+++ setup.py
@@ -6,37 +6,38 @@ __version__ = (3, 2, 0)
 
 
 class PyTest(TestCommand):
-
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = ['-v', 'tests']
+        self.test_args = ["-v", "tests"]
         self.test_suite = True
 
     def run_tests(self):
         # import here, cause outside the eggs aren't loaded
         import pytest
+
         errno = pytest.main(self.test_args)
         sys.exit(errno)
+
 
 setup(
     name="PyCDS",
     description="An ORM representation of the PCDS and CRMP database",
     keywords="sql database pcds crmp climate meteorology",
-    packages=find_packages(include=['pycds*']),
-    version='.'.join(str(d) for d in __version__),
+    packages=find_packages(include=["pycds*"]),
+    version=".".join(str(d) for d in __version__),
     url="http://www.pacificclimate.org/",
     author="James Hiebert",
     author_email="hiebert@uvic.ca",
-    package_data={'pycds': ['data/crmp_subset_data.sql']},
+    package_data={"pycds": ["data/crmp_subset_data.sql"]},
     include_package_data=True,
     zip_safe=True,
     scripts=[
-        'scripts/demo.py',
-        'scripts/mktestdb.py',
-        'scripts/format-pytest.py',
-        'scripts/load-climate-baseline-values.py',
-        'scripts/verify-load-climate-baseline-values.py',
-        'scripts/manage-views.py'
+        "scripts/demo.py",
+        "scripts/mktestdb.py",
+        "scripts/format-pytest.py",
+        "scripts/load-climate-baseline-values.py",
+        "scripts/verify-load-climate-baseline-values.py",
+        "scripts/manage-views.py",
     ],
     install_requires=[
         "alembic",
@@ -51,11 +52,10 @@ setup(
         "pytest-mock",
         "testing.postgresql",
         "alembic",
-        "alembic-verify"
+        "alembic-verify",
     ],
-    cmdclass={'test': PyTest},
-
-    classifiers='''Development Status :: 5 - Production/Stable
+    cmdclass={"test": PyTest},
+    classifiers="""Development Status :: 5 - Production/Stable
 Environment :: Console
 Intended Audience :: Developers
 Intended Audience :: Science/Research
@@ -66,5 +66,7 @@ Programming Language :: Python :: 3.7
 Topic :: Internet
 Topic :: Scientific/Engineering
 Topic :: Database
-Topic :: Software Development :: Libraries :: Python Modules'''.split('\n')
+Topic :: Software Development :: Libraries :: Python Modules""".split(
+        "\n"
+    ),
 )
