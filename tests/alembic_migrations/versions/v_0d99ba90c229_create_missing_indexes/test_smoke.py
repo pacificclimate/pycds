5fa0eaf9f932acd5c589a65f0d5658dca9d2fc24
rod-glover <rod.glover.ca@gmail.com>
2021-05-26 13:11:06 -0700

    Update 0d99 tests

--- tests/alembic_migrations/versions/v_0d99ba90c229_create_missing_indexes/test_smoke.py
+++ tests/alembic_migrations/versions/v_0d99ba90c229_create_missing_indexes/test_smoke.py
@@ -22,7 +22,7 @@ table_and_index = (
     ("meta_history", "fki_meta_history_station_id_fk"),
     ("meta_station", "fki_meta_station_network_id_fkey"),
     ("meta_vars", "fki_meta_vars_network_id_fkey"),
-    ("meta_climo_attrs", "meta_climo_attrs_idx"),
+    # ("meta_climo_attrs", "meta_climo_attrs_idx"),
     ("meta_history", "meta_history_freq_idx"),
     ("obs_count_per_month_history_mv", "obs_count_per_month_history_idx"),
     ("station_obs_stats_mv", "station_obs_stats_mv_idx"),
@@ -31,7 +31,7 @@ table_and_index = (
 
 @pytest.mark.usefixtures("new_db_left")
 @pytest.mark.parametrize(
-    "prepared_schema_from_migrations_left", ("e688e520d265",), indirect=True
+    "prepared_schema_from_migrations_left", ("2914c6c8a7f9",), indirect=True
 )
 @pytest.mark.parametrize("pre_create", [tuple(), (History, Station, Variable)])
 def test_upgrade(
@@ -40,9 +40,9 @@ def test_upgrade(
     schema_name,
     pre_create,
 ):
-    """Test the schema migration from e688e520d265 to 0d99ba90c229."""
+    """Test the schema migration from 2914c6c8a7f9 to 0d99ba90c229."""
 
-    # Set up database to revision e688e520d265
+    # Set up database to revision 2914c6c8a7f9
     engine, script = prepared_schema_from_migrations_left
 
     # Pre-create some indexes to exercise "if not exists"
@@ -64,12 +64,12 @@ def test_upgrade(
 def test_downgrade(
     prepared_schema_from_migrations_left, alembic_config_left, schema_name
 ):
-    """Test the schema migration from 0d99ba90c229 to e688e520d265."""
+    """Test the schema migration from 0d99ba90c229 to 2914c6c8a7f9."""
 
     # Set up database to revision 0d99ba90c229
     engine, script = prepared_schema_from_migrations_left
 
-    # Downgrade to revision e688e520d265
+    # Downgrade to revision 2914c6c8a7f9
     command.downgrade(alembic_config_left, "-1")
 
     # Check that indexes have been removed
