bc54fe4793e1f47b3c7c578a0eee9d07b5fe2708
Johnathan Helfrich <helfy16@telus.net>
2022-01-14 08:55:17 -0800

    Fulfill requested changes

--- tests/alembic_migrations/test_check_migration_version.py
+++ tests/alembic_migrations/test_check_migration_version.py
@@ -7,7 +7,7 @@ from pycds.database import check_migration_version
 
 
 def test_get_current_head():
-    assert get_current_head() == "879f0efa125f"
+    assert get_current_head() == "3d50ec832e47"
 
 
 @pytest.mark.usefixtures("new_db_left")
