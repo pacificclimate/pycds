4a7b0eb172b090d39a801805979114a50b724de4
Johnathan Helfrich <helfy16@telus.net>
2022-01-17 15:47:06 -0800

    Add not null constraint to table
    Add not null constraints to three variables in the meta_vars table
    modify the subset data so that the constraint is not violated

---
+++ pycds/alembic/versions/7b139906ac46_add_not_null_constraint_on_meta_vars.py
@@ -0,0 +1,24 @@
+"""add not null constraint on meta vars
+
+Revision ID: 7b139906ac46
+Revises: 3d50ec832e47
+Create Date: 2022-01-17 14:21:37.975335
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "7b139906ac46"
+down_revision = "3d50ec832e47"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
