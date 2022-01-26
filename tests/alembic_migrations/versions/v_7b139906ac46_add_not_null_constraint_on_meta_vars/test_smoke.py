742b1459c59a024c2a6b95e30a9c22f5f0d06755
Johnathan Helfrich <helfy16@telus.net>
2022-01-26 11:27:00 -0800

    add data migration test to upgrade

--- tests/alembic_migrations/versions/v_7b139906ac46_add_not_null_constraint_on_meta_vars/test_smoke.py
+++ tests/alembic_migrations/versions/v_7b139906ac46_add_not_null_constraint_on_meta_vars/test_smoke.py
@@ -11,14 +11,26 @@ from sqlalchemy import inspect
 
 logger = logging.getLogger("tests")
 
+@pytest.mark.parametrize(
+        "prepared_schema_from_migrations_left", ("3d50ec832e47",),indirect=True
+)
 @pytest.mark.usefixtures("new_db_left")
-def test_upgrade(prepared_schema_from_migrations_left, schema_name):
+def test_upgrade(
+    prepared_schema_from_migrations_left, alembic_config_left, schema_name
+):
     """test migration from 3d50ec832e47 to 7b139906ac46. """
     
     # Set up database to version 7b139906ac46
     engine, script = prepared_schema_from_migrations_left
 
-    # Check that views have been added
+    engine.execute(
+        f"INSERT INTO {schema_name}.meta_vars(vars_id)"
+        f"VALUES (1)"
+    )
+
+    command.upgrade(alembic_config_left, "+1")
+
+    # Check that not null constraints have been added
     table = inspect(engine).get_columns("meta_vars", schema=schema_name)
 
     for col in table:
@@ -38,7 +50,7 @@ def test_downgrade(
     # Run downgrade migration
     command.downgrade(alembic_config_left, "-1")
 
-    # Check that views have been removed
+    # Check that not null constraints have been removed
     table = inspect(engine).get_columns("meta_vars", schema=schema_name)
     
     for col in table:
