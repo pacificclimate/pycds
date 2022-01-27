7fd7bfdd64b2d1d15bd54d8d3c602a6156db6da5
Johnathan Helfrich <helfy16@telus.net>
2022-01-27 11:20:13 -0800

    add test of data migration

--- tests/alembic_migrations/versions/v_7b139906ac46_add_not_null_constraint_on_meta_vars/test_smoke.py
+++ tests/alembic_migrations/versions/v_7b139906ac46_add_not_null_constraint_on_meta_vars/test_smoke.py
@@ -7,7 +7,7 @@
 import logging
 import pytest
 from alembic import command
-from sqlalchemy import inspect
+from sqlalchemy import inspect, text
 
 logger = logging.getLogger("tests")
 
@@ -30,6 +30,16 @@ def test_upgrade(
 
     command.upgrade(alembic_config_left, "+1")
 
+    # Check that data has been modified to remove nulls
+    result = engine.execute(
+        f"SELECT * FROM {schema_name}.meta_vars"
+    )
+    for row in result:
+        if row.vars_id == 1:
+            assert row.cell_method == "foo: bar"
+            assert row.standard_name == "foo_bar"
+            assert row.display_name == "foo bar"
+
     # Check that not null constraints have been added
     table = inspect(engine).get_columns("meta_vars", schema=schema_name)
 
@@ -47,12 +57,28 @@ def test_downgrade(
     # Set up database to version 7b139906ac46
     engine, script = prepared_schema_from_migrations_left
 
+    engine.execute(
+        f"INSERT INTO {schema_name}.meta_vars(vars_id, cell_method, standard_name, display_name)"
+        f"VALUES (10000, 'foo: bar', 'foo_bar', 'foo bar')"
+    )
+
     # Run downgrade migration
     command.downgrade(alembic_config_left, "-1")
 
+    # Check that data has been modified to insert nulls back in
+
+    result = engine.execute(
+        f"SELECT * FROM {schema_name}.meta_vars"
+    )
+    for row in result:
+        if row.vars_id == 10000:
+            assert row.cell_method == None
+            assert row.standard_name == None
+            assert row.display_name == None
+
     # Check that not null constraints have been removed
     table = inspect(engine).get_columns("meta_vars", schema=schema_name)
-    
+
     for col in table:
         if col["name"]=="cell_method" or col["name"]=="standard_name" or col["name"]=="display_name":
             assert(col["nullable"]==True)
