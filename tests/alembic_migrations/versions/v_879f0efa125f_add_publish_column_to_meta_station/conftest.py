21d1e031a30b16320adcacfe7711ead546ca8f19
Ada Sungar <adasungar@hotmail.com>
2022-01-27 16:58:41 -0800

    Tests for publish column migration

---
+++ tests/alembic_migrations/versions/v_879f0efa125f_add_publish_column_to_meta_station/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.fixture(scope="module")
+def target_revision():
+    return "879f0efa125f"
---
+++ tests/alembic_migrations/versions/v_879f0efa125f_add_publish_column_to_meta_station/test_smoke.py
@@ -0,0 +1,63 @@
+"""Smoke tests:
+- Upgrade adds publish attribute to meta station table
+- Downgrade drops publish attirbute from meta station table
+"""
+
+# -*- coding: utf-8 -*-
+import logging
+import pytest
+from sqlalchemy import inspect, null
+from alembic import command
+
+
+logger = logging.getLogger("tests")
+
+column_name = "publish"
+table_name = "meta_station"
+
+def check_if_column_exists(col_name, sch_cols):
+    for col in sch_cols:
+        if col["name"] == col_name:
+            return col
+    return null
+
+@pytest.mark.usefixtures("new_db_left")
+@pytest.mark.parametrize(
+    "prepared_schema_from_migrations_left", ("3d50ec832e47",), indirect=True
+)
+def test_upgrade(
+    prepared_schema_from_migrations_left,
+    alembic_config_left,
+    schema_name,
+):
+    """Test the schema migration from 3d50ec832e47 to 879f0efa125f."""
+
+    # Set up database to revision 3d50ec832e47
+    engine, script = prepared_schema_from_migrations_left
+
+    # Upgrade to 879f0efa125f
+    command.upgrade(alembic_config_left, "879f0efa125f")
+
+    # Check that column has been added to meta_station
+    meta_station_table = inspect(engine).get_columns(table_name, schema=schema_name)
+    col = check_if_column_exists(column_name, meta_station_table)
+
+    assert (col["nullable"] == False) and (col["name"] == column_name)
+
+
+@pytest.mark.usefixtures("new_db_left")
+def test_downgrade(
+    prepared_schema_from_migrations_left, alembic_config_left, schema_name):
+    """Test the schema migration from 879f0efa125f to 3d50ec832e47."""
+
+    # Set up database to revision 879f0efa125f
+    engine, script = prepared_schema_from_migrations_left
+
+    # Downgrade to revision 3d50ec832e47
+    command.downgrade(alembic_config_left, "-1")
+
+    # Check that cloumn has been removed from meta_station
+    meta_station_table = inspect(engine).get_columns(table_name, schema=schema_name)
+    col = check_if_column_exists(column_name, meta_station_table)
+
+    assert (col == null)
