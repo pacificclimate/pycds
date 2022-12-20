292f8f59d94e3b1fedb558f64803f64dc47b1551
rod-glover <rod.glover.ca@gmail.com>
2022-12-19 16:59:17 -0800

    Add get_cxhx_schema_name fn

--- pycds/context.py
+++ pycds/context.py
@@ -5,5 +5,9 @@ def get_schema_name():
     return os.environ.get("PYCDS_SCHEMA_NAME", "crmp")
 
 
+def get_cxhx_schema_name():
+    return os.environ.get("PYCDS_CXHX_SCHEMA_NAME", f"{get_schema_name()}_cxhx")
+
+
 def get_su_role_name():
     return os.environ.get("PYCDS_SU_ROLE_NAME", "pcicdba")
