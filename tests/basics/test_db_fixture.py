6b8910069d6ad92ff5910142b0436b72e6a1aee9
rod-glover <rod.glover.ca@gmail.com>
2022-12-22 14:44:43 -0800

    Tighten PostGIS version check

--- tests/basics/test_db_fixture.py
+++ tests/basics/test_db_fixture.py
@@ -1,4 +1,4 @@
 def test_can_create_postgis_db(empty_sesh):
     res = empty_sesh.execute("SELECT PostGIS_full_version()")
     version = res.fetchall()[0][0]
-    assert 'POSTGIS="2.' in version or 'POSTGIS="3.' in version
+    assert 'POSTGIS="3.' in version
