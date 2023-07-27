92ea49d78bca6306377897e735d34158a5f0188a
rod-glover <rod.glover.ca@gmail.com>
2023-07-27 16:15:19 -0700

    Handle OR REPLACE in DDL extension CREATE VIEW

--- pycds/sqlalchemy/ddl_extensions/view.py
+++ pycds/sqlalchemy/ddl_extensions/view.py
@@ -3,15 +3,21 @@ from ..ddl_extensions.view_common import ViewCommonDDL
 
 
 class CreateView(ViewCommonDDL):
-    pass
+    def __init__(self, name, selectable=None, replace=False):
+        super().__init__(name, selectable)
+        self.replace = replace
 
 
 @compiler.compiles(CreateView)
 def compile(element, compiler, **kw):
     # TODO: Add escape
-    # TODO: Handle replace
     body = compiler.sql_compiler.process(element.selectable, literal_binds=True)
-    return f"CREATE VIEW {element.name} AS {body}"
+    command_parts = [
+        "CREATE",
+        element.replace and "OR REPLACE",
+        f"VIEW {element.name} AS  {body}",
+    ]
+    return " ".join(filter(None, command_parts))
 
 
 class DropView(ViewCommonDDL):
