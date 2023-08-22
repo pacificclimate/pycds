4753b4c0cda6d7c2dc6ef9a34da2388ac4c65668
rod-glover <rod.glover.ca@gmail.com>
2023-08-22 11:28:42 -0700

    Add behavioural test for ClimoObsCount

---
+++ tests/behavioural/native_matviews/test_climo_obs_count.py
@@ -0,0 +1,58 @@
+import pytest
+import sqlalchemy
+from pycds.orm.native_matviews import ClimoObsCount
+
+
+@pytest.mark.usefixtures("new_db_left")
+def test_matview_content(sesh_with_large_data):
+    """Test that ClimoObsCount definition is correct."""
+
+    # No content before matview is refreshed
+    q = sesh_with_large_data.query(ClimoObsCount)
+    assert q.count() == 0
+
+    # Refresh
+    sesh_with_large_data.execute(ClimoObsCount.refresh())
+
+    # Et voila
+    assert q.count() > 0
+
+    # This test sucks, relying as it does on hardcoded magic numbers taken from
+    # the large dataset. But it's what we've got just now.
+    expected_pairs = {
+        (48, 816),
+        (48, 1516),
+        (2, 2516),
+        (48, 1216),
+        (48, 1016),
+        (48, 1616),
+        (12, 516),
+        (36, 1816),
+        (48, 1716),
+        (48, 1316),
+        (21, 3516),
+        (5, 2016),
+    }
+    result_pairs = {(row.count, row.history_id) for row in q.all()}
+    assert expected_pairs <= result_pairs
+    for pair in expected_pairs:
+        assert pair in result_pairs
+
+
+@pytest.mark.usefixtures("new_db_left")
+def test_index(schema_name, prepared_schema_from_migrations_left):
+    """Test that ClimoObsCount has the expected index."""
+    engine, script = prepared_schema_from_migrations_left
+    inspector = sqlalchemy.inspect(engine)
+    indexes = inspector.get_indexes(
+        table_name=(ClimoObsCount.base_name()), schema=schema_name
+    )
+    assert indexes == [
+        {
+            "name": "climo_obs_count_idx",
+            "column_names": ["history_id"],
+            "unique": False,
+            "include_columns": [],
+            "dialect_options": {"postgresql_include": []},
+        }
+    ]
