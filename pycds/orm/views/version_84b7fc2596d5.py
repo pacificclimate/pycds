c33ca5865183c20abd6350149dc5a03672b22aaf
rod-glover <rod.glover.ca@gmail.com>
2023-08-22 17:41:21 -0700

    Fix import of CollapsedVars

--- pycds/orm/views/version_84b7fc2596d5.py
+++ pycds/orm/views/version_84b7fc2596d5.py
@@ -29,9 +29,8 @@ from pycds.orm.tables import (
     History,
     Variable,
     Obs,
-    CollapsedVariables,
 )
-from pycds.orm.native_matviews import StationObservationStats
+from pycds.orm.native_matviews import StationObservationStats, CollapsedVariables
 from pycds.alembic.extensions.replaceable_objects import ReplaceableView
 from pycds.orm.view_base import Base
 
