ccc79c1b0d057136d14e7da1901acc83b97175f6
rod-glover <rod.glover.ca@gmail.com>
2024-01-10 11:07:53 -0800

    Fix bug in func definition

--- pycds/orm/functions/version_efde19ea4f52.py
+++ pycds/orm/functions/version_efde19ea4f52.py
@@ -35,15 +35,15 @@ getstationvariabletable = ReplaceableFunction(
     AS $BODY$
         query = (
             "SELECT vars_id, net_var_name " 
-            "FROM meta_vars JOIN meta_station ON meta_vars.network_id = meta_station.network_id " 
-            "WHERE ARRAY['" + ("climatology" if climo else "observation") + "'::text] <@ variable_tags(meta_vars.*) " 
+            "FROM {schema_name}.meta_vars JOIN {schema_name}.meta_station ON {schema_name}.meta_vars.network_id = {schema_name}.meta_station.network_id " 
+            "WHERE ARRAY['" + ("climatology" if climo else "observation") + "'::text] <@ {schema_name}.variable_tags({schema_name}.meta_vars.*) " 
             "AND station_id = " + str(station_id)
             + " ORDER BY net_var_name" 
         )
         data = plpy.execute(query)
         bits = [ 'obs_time' ] + [ ("MAX(CASE WHEN vars_id=" + str(x['vars_id']) + " THEN datum END) as " + x['net_var_name']) for x in data ]
         vars_ids = [ str(x['vars_id']) for x in data ]
-        hid_query = "SELECT history_id FROM meta_history WHERE station_id=" + str(station_id)
+        hid_query = "SELECT history_id FROM {schema_name}.meta_history WHERE station_id=" + str(station_id)
         hid_data = plpy.execute(hid_query)
         hid_clauses = [ "history_id = " + str(x['history_id']) for x in hid_data ]
         return "SELECT "+ ",".join(bits) + " from obs_raw WHERE (" + " OR ".join(hid_clauses) + ") AND vars_id IN (" + ",".join(vars_ids) + ") GROUP BY obs_time ORDER BY obs_time" 
