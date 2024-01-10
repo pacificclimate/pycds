d7266def4082584e7a583df7aa58b8d8e5f10eb4
rod-glover <rod.glover.ca@gmail.com>
2024-01-09 16:47:23 -0800

    Add skeleton migration

---
+++ pycds/alembic/versions/efde19ea4f52_fix_function_getstationvariabletable.py
@@ -0,0 +1,24 @@
+"""Fix function getstationvariabletable
+
+Revision ID: efde19ea4f52
+Revises: 6cb393f711c3
+Create Date: 2024-01-09 16:22:39.469951
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "efde19ea4f52"
+down_revision = "6cb393f711c3"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
