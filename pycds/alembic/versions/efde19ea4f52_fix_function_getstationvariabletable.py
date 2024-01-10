3cbb8da4e7b4df48d886d94f60470befdf6d0a54
rod-glover <rod.glover.ca@gmail.com>
2024-01-09 17:12:25 -0800

    Complete migration script

--- pycds/alembic/versions/efde19ea4f52_fix_function_getstationvariabletable.py
+++ pycds/alembic/versions/efde19ea4f52_fix_function_getstationvariabletable.py
@@ -8,6 +8,12 @@ Create Date: 2024-01-09 16:22:39.469951
 from alembic import op
 import sqlalchemy as sa
 
+from pycds.orm.functions.version_4a2f1879293a import (
+    getstationvariabletable as old_getstationvariabletable,
+)
+from pycds.orm.functions.version_efde19ea4f52 import (
+    getstationvariabletable as new_getstationvariabletable,
+)
 
 # revision identifiers, used by Alembic.
 revision = "efde19ea4f52"
@@ -17,8 +23,10 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    op.drop_replaceable_object(old_getstationvariabletable)
+    op.create_replaceable_object(new_getstationvariabletable)
 
 
 def downgrade():
-    pass
+    op.drop_replaceable_object(new_getstationvariabletable)
+    op.create_replaceable_object(old_getstationvariabletable)
