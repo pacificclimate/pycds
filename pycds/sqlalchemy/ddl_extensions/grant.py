efe8910ee1a4c77d5f15b4c6ccc7a661fd0d1b91
rod-glover <rod.glover.ca@gmail.com>
2024-01-24 16:23:06 -0800

    Add DDL extension for GRANT commands

---
+++ pycds/sqlalchemy/ddl_extensions/grant.py
@@ -0,0 +1,23 @@
+"""
+DDLElements for SQL GRANT statements.
+"""
+
+from sqlalchemy.schema import DDLElement
+from sqlalchemy.ext import compiler
+
+
+class GrantTablePrivileges(DDLElement):
+    """"""
+    def __init__(self, privileges, table_name, role_specification, schema=None):
+        self.privileges = privileges
+        self.table_name = table_name
+        self.role_specification = role_specification
+        self.schema = schema
+
+@compiler.compiles(GrantTablePrivileges)
+def compile_grant_table_privileges(element, compiler, **kw):
+    privileges = ", ".join(element.privileges)
+    schema_prefix = f"{element.schema}." if element.schema is not None else ""
+    object_id = f"{schema_prefix}{element.table_name}"
+    return f"GRANT {privileges} ON {object_id} TO {element.role_specification}"
+
