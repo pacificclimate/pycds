9d51fc44a523572b2bdf0d29cf5b6c7bf4743405
rod-glover <rod.glover.ca@gmail.com>
2024-01-25 17:00:56 -0800

    Define create/drop view/matview fns with grant privs

--- pycds/alembic/util.py
+++ pycds/alembic/util.py
@@ -1,8 +1,40 @@
 from typing import List, Union, Tuple
 
 from sqlalchemy.sql.ddl import DDLElement
-
 from alembic import op
+
+
+def create_view_or_matview(obj, schema=None, grant_privs=True, create_indexes=True):
+    """Drop a matview, and optionally create the indexes associated with it. """
+    # Create the matview
+    op.create_replaceable_object(obj, schema=schema)
+    if grant_privs:
+        grant_standard_table_privileges(obj, schema=schema)
+    if create_indexes:
+        # Create any indices on the matview
+        for index in obj.__table__.indexes:
+            op.create_index(
+                index_name=index.name,
+                table_name=index.table.name,
+                columns=[col.name for col in index.columns],
+                unique=index.unique,
+                schema=schema,
+            )
+
+
+def drop_view_or_matview(obj, schema=None, drop_indexes=True):
+    """Drop a matview, and optionally drop the indexes associated with it. """
+    if drop_indexes:
+        # Drop any indexes on the matview
+        for index in obj.__table__.indexes:
+            op.drop_index(
+                index_name=index.name,
+                table_name=index.table.name,
+                schema=schema,
+            )
+    # Drop the matview
+    op.drop_replaceable_object(obj, schema=schema)
+
 
 
 def grant_standard_table_privileges(
@@ -14,7 +46,7 @@ def grant_standard_table_privileges(
     ),
     schema: str = None,
 ) -> None:
-    """Grant standard privileges on a table-like object (table, view, matview).
+    """Grant standard privileges on a table-like object (table, view, table_object).
 
     :param obj: Table-like ORM object or string identifying the object permissions
         are to be granted on.
