db7b4f696143423fbc7143f4d5e34926f203fd01
rod-glover <rod.glover.ca@gmail.com>
2024-01-29 17:20:08 -0800

    Blacken

--- pycds/context.py
+++ pycds/context.py
@@ -6,7 +6,8 @@ def split_on(sep):
     def f(s):
         if s == "":
             return []
-        return re.split(fr"\s*{sep}\s*", s)
+        return re.split(rf"\s*{sep}\s*", s)
+
     return f
 
 
@@ -17,24 +18,23 @@ def parse_standard_table_privileges(stp):
     Privileges are separated by commas. A sequence of such role-priv items is separated
     by semicolons.
     """
-    split_items = split_on(';')
-    split_item = split_on(':')
+    split_items = split_on(";")
+    split_item = split_on(":")
 
     def split_privs(rp):
-        return rp[0], split_on(',')(rp[1])
+        return rp[0], split_on(",")(rp[1])
 
     return list(map(split_privs, map(split_item, split_items(stp))))
 
 
 def get_standard_table_privileges(
-    default_privs="inspector: SELECT; viewer: SELECT; steward: ALL"
+    default_privs="inspector: SELECT; viewer: SELECT; steward: ALL",
 ):
     """Get and parse an environment variable defining the standard privileges to be
     applied to new table-like) objects (tables, views, matviews.
     """
     env = os.environ.get("PYCDS_STANDARD_TABLE_PRIVS", default_privs)
     return parse_standard_table_privileges(env)
-
 
 
 def get_schema_name():
