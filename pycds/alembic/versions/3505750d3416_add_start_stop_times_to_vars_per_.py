6b1b95bea5ce9dd97e8a1f9054a8cbb356b67716
Lee Zeman <lzeman@uvic.ca>
2024-03-05 13:50:59 -0800

    update existing tests

--- pycds/alembic/versions/3505750d3416_add_start_stop_times_to_vars_per_.py
+++ pycds/alembic/versions/3505750d3416_add_start_stop_times_to_vars_per_.py
@@ -28,40 +28,54 @@ revision = "3505750d3416"
 down_revision = "efde19ea4f52"
 branch_labels = None
 depends_on = None    
-    
-    
+
+# drop other objects that depend on the view being upgraded
+def drop_dependent_objects():
+    op.drop_replaceable_object(CrmpNetworkGeoserver)
+    op.drop_replaceable_object(CollapsedVariables)
+
+#recreate other objects that depend on the view being upgraded
+def create_dependent_objects():
+    op.create_replaceable_object(CollapsedVariables)
+    op.create_replaceable_object(CrmpNetworkGeoserver)
+    op.create_index_if_not_exists(index_name = "collapsed_vars_idx", 
+                                  table_name = "collapsed_vars_mv", 
+                                  columns = ["history_id"],
+                                  unique = False,
+                                  schema = "crmp")
+                                  
+def create_var_hist_index():
+    op.create_index_if_not_exists(index_name = "var_hist_idx",
+                                  table_name = "vars_per_history_mv",
+                                  columns = ["history_id", "vars_id"],
+                                  unique = False,
+                                  schema = "crmp")
+
 def upgrade():
     op.set_role(get_su_role_name())
     
-    #drop dependent objects
-    op.drop_replaceable_object(CrmpNetworkGeoserver)
-    op.drop_replaceable_object(CollapsedVariables)
+    drop_dependent_objects()
     
-    #upgrade VarsPerHistory
     op.drop_replaceable_object(old_varsperhistory)
     op.create_replaceable_object(new_varsperhistory)
+    #why does this have to be done explicitly?
+    create_var_hist_index()
     
-    #restore dependent objects
-    op.create_replaceable_object(CollapsedVariables)
-    op.create_replaceable_object(CrmpNetworkGeoserver)
+    create_dependent_objects()
 
     op.reset_role()
 
 
 def downgrade():
     op.set_role(get_su_role_name())
+
+    drop_dependent_objects()
     
-    #drop dependent objects
-    op.drop_replaceable_object(CrmpNetworkGeoserver)
-    op.drop_replaceable_object(CollapsedVariables)
-    
-    #downgrade VarsPerHistory
     op.drop_replaceable_object(new_varsperhistory)
     op.create_replaceable_object(old_varsperhistory)
+    create_var_hist_index()
     
-    #restore dependent objects
-    op.create_replaceable_object(CollapsedVariables)
-    op.create_replaceable_object(CrmpNetworkGeoserver)
+    create_dependent_objects()
 
     op.reset_role()
 
--- tests/alembic_migrations/test_check_migration_version.py
+++ tests/alembic_migrations/test_check_migration_version.py
@@ -7,7 +7,7 @@ from pycds.database import check_migration_version
 
 
 def test_get_current_head():
-    assert get_current_head() == "efde19ea4f52"
+    assert get_current_head() == "3505750d3416"
 
 
 @pytest.mark.usefixtures("new_db_left")
