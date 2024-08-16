3248113452b340e6cdd7e5a9b970cb57014589ee
rod-glover <rod.glover.ca@gmail.com>
2024-08-16 15:57:54 -0700

    Add skeleton migration

---
+++ pycds/alembic/versions/434b4a868241_add_metadata_change_history.py
@@ -0,0 +1,24 @@
+"""Add metadata change history
+
+Revision ID: 434b4a868241
+Revises: 081f17262852
+Create Date: 2024-08-16 15:47:28.844217
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "434b4a868241"
+down_revision = "081f17262852"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
