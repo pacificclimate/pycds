20337060365ad4f2f869e4d033c709f0c43e24fd
rod-glover <rod.glover.ca@gmail.com>
2024-08-20 11:48:39 -0700

    Improve documentation

--- pycds/orm/trigger_functions/version_434b4a868241.py
+++ pycds/orm/trigger_functions/version_434b4a868241.py
@@ -1,45 +1,60 @@
 """
 Define trigger function ``update_metadata_generic()``.
 
-This trigger function is the core of the metadata change history tracking code. It is used by triggers on the metadata
-views that form the user-facing interface of the metadata. This trigger function (plus the appropriate CREATE
-TRIGGER statements) cause the views to have a table-like interface supporting INSERT, UPDATE and DELETE operations.
+This trigger function is the core of the metadata change history tracking code.
+It is used by triggers on the metadata views that form the user-facing interface of the metadata. This trigger function
+(plus the appropriate CREATE TRIGGER statements) cause the views to have a
+table-like interface supporting INSERT, UPDATE and DELETE operations.
 
 View-History table setup
 ========================
 
 For each metadata "table", a coordinated view, sequence, and table are defined:
 
-* The view provides the user-facing interface to the "table". Triggers are attached to the view to handle INSERT,
-  UPDATE and DELETE operations.
+* The view provides the user-facing interface to the "table". Triggers are
+  attached to the view to handle INSERT, UPDATE and DELETE operations.
 * Behind the metadata view is the metadata history table.
-* The history table is append-only, and contains records of every past and present state of the metadata.
+* The history table is append-only, and contains records of every past and
+  present state of the metadata.
 
-    * Each row of the history table represents a single state (in time) of a single metadata item.
-    * The view selects the latest metadata item from history table. Thus the user sees only the latest version of each metadata item.
+    * Each row of the history table represents a single state (in time) of a
+      single metadata item.
+    * The view selects the latest metadata item from history table. Thus the
+      user sees only the latest version of each metadata item.
 
 * The sequence provides the unique primary key for the history table.
 
 Trigger function for metadata views
 ===================================
 
-As noted above, triggers are defined on each metadata view to convert insert, update, and delete operations on the
-view to inserts (only) on the history table.
+Triggers are defined on each metadata view to convert insert, update, and
+delete operations on the view to inserts (only) on the history table.
 
-We define one generic trigger function that will work for all the different metadata tables, rather than having to
-define a separate trigger function for each table. The parametrization of trigger functions allows us to do that.
+We define one generic trigger function that works for all metadata tables,
+rather than having to define a custom trigger function for each table.
 
-It's important to keep in mind that there are two id's in play in the history table:
+Note
+    The parametrization of trigger functions permits us to generalize the
+    trigger function: A trigger function is in fact a function factory or
+    higher-order function that returns the actual function called by any given
+    trigger. That function is created when the CREATE TRIGGER statement is
+    processed, and it arguments to the factory can be passed in the statement.
 
-* The *metadata history* id, which is a unique identifier for the record. It is provided by a corresponding sequence.
-* The *metadata* id, which identifies a single metadata item, but which can have many timestamped history records for
-  it. The combination of metadata id and timestamp is unique. (This pair could in theory replace the history id, but
-  implementation is much simpler if we tolerate this slight lack of normalization.)
+It's important to keep in mind that there are two id's in play in the history
+table:
 
-The secret sauce is in the INSERT statement that the trigger function prepares and executes. It works only if the
-following conditions are met:
+* The *metadata history* id, which is a unique identifier for the record. It
+  is provided by a corresponding sequence.
+* The *metadata* id, which identifies a single metadata item, but which can
+  have many timestamped history records for it. The combination of metadata id
+  and timestamp is unique. (This pair could in theory replace the history id,
+  but implementation is much simpler if we tolerate this slight lack of
+  normalization.)
 
-* The view, history table, and sequence, must be named as follows:
+The secret sauce is in the INSERT statement that the trigger function prepares
+and executes. It works only if the following conditions are met:
+
+* The view, history table and sequence must be named as follows:
 
     * View: ``<view_name>``
     * History table: ``<view_name>_hx``
@@ -56,16 +71,19 @@ following conditions are met:
 
     * metadata columns proper
 
-        * metadata id (e.g., network_id, vars_id, station_id, history_id in existing metadata tables)
+        * metadata id (e.g., network_id, vars_id, station_id, history_id in
+          existing metadata tables)
         * metadata-specific columns, which vary by metadata table
 
 * View column *names* may be changed by the view definition,
-  but the view must consist of exactly the following columns in the following order:
+  but the view must consist of exactly the following columns in the following
+  order:
 
     * create time (a.k.a. mod time)
     * creator (id of user causing this record to be created)
     * metadata id
-    * metadata-specific columns, which vary by metadata table, in the same order as the history table
+    * metadata-specific columns, which vary by metadata table, in the same
+      order as the history table
 
 The INSERT statement is prepared and executed in two phases:
 
@@ -73,23 +91,29 @@ The INSERT statement is prepared and executed in two phases:
 * Later, column arguments are substituted when it is EXECUTEd.
 * The final result is an INSERT statement of the following form::
 
-    INSERT INTO <history table> VALUES <metadata history id> <deleted> <create time> <creator> <metadata id> <metadata-specific columns...>
+    INSERT INTO <history table> VALUES <metadata history id> <deleted>
+      <create time> <creator> <metadata id> <metadata-specific columns...>
 
-* The first two columns are provided explicitly, as parameters ``$1``, ``$2``.
-* The remaining columns are provided by spreading either the NEW or the OLD record, depending on what operation caused
-  the trigger. The whole record is passed as parameter ``$3``.
-* We have to update the following information in the ``NEW``/``OLD`` record before it can be passed to the EXECUTE
-  statement:
+    * The first two columns are provided explicitly, as parameters
+      ``$1``, ``$2``.
+    * The remaining columns are provided by spreading either the ``NEW`` or
+      the ``OLD`` record, depending on what operation caused the trigger.
+      This record is passed as parameter ``$3``.
+
+* We must update the following information in the ``NEW``/``OLD`` record
+  before it can be passed to the EXECUTE statement:
 
     * create time
     * creator
-    * metadata id – in the case of a newly created metadata item, it is the same as the history id; in the case of an
-      existing metadata item, it is brought forward from the existing metadata item
+    * metadata id – in the case of a newly created metadata item, it is the
+      same as the history id; in the case of an existing metadata item, it is
+      brought forward from the existing metadata item
 
-To do some of the manipulations we require (see prev item) on the ``NEW``/``OLD`` records, we must access their contents by
-attribute name (specifically, metadata id column name). By far the easiest way to do this in ``pgplsql`` is to use the
-``hstore`` extension. Its syntax and usage are slightly gnarly, but it is very helpful. For more details on
-``hstore``, see PG documentation .
+    To do some of these manipulations on the ``NEW``/``OLD`` records, we must
+    access their contents by attribute name (specifically, metadata id column
+    name). By far the easiest way to do this in ``pgplsql`` is to use the
+    ``hstore`` extension. Its syntax and usage are slightly gnarly, but it is
+    very helpful. For more details on ``hstore``, see PG documentation .
 """
 from pycds.alembic.extensions.replaceable_objects import ReplaceableFunction
 from pycds.context import get_schema_name
