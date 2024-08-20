cbff3c1cf728a80bd2c0185eb198fbdba6e768f7
rod-glover <rod.glover.ca@gmail.com>
2024-08-19 17:57:59 -0700

    Update migration to add trigger fn

--- pycds/alembic/versions/434b4a868241_add_metadata_change_history.py
+++ pycds/alembic/versions/434b4a868241_add_metadata_change_history.py
@@ -7,6 +7,8 @@ Create Date: 2024-08-16 15:47:28.844217
 """
 from alembic import op
 import sqlalchemy as sa
+from pycds.context import get_su_role_name
+from pycds.orm.trigger_functions.version_434b4a868241 import update_metadata
 
 
 # revision identifiers, used by Alembic.
@@ -17,8 +19,10 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    op.set_role(get_su_role_name())
+    op.create_replaceable_object(update_metadata)
+    op.reset_role()
 
 
 def downgrade():
-    pass
+    op.drop_replaceable_object(update_metadata)
