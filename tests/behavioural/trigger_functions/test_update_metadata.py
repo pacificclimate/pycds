7313dbecf9525dfedd4ea28e0741bad447349ded
rod-glover <rod.glover.ca@gmail.com>
2024-08-20 12:07:08 -0700

    Clean up test for update_metdata

--- tests/behavioural/trigger_functions/test_update_metadata.py
+++ tests/behavioural/trigger_functions/test_update_metadata.py
@@ -29,6 +29,7 @@ from pycds import get_schema_name
         ),
     ],
 )
+@pytest.mark.parametrize("print_info", [True])
 def test_update_metadata(
     sesh_in_prepared_schema_left,
     setup_metadata_objects,
@@ -37,15 +38,25 @@ def test_update_metadata(
     metadata_cols,
     operations,
     final_metadata,
+    print_info,
 ):
     sesh = sesh_in_prepared_schema_left
 
-    def execute(s):
+    def execute(s, print_=False):
         result = sesh.execute(text(s))
         # Commit is required to ensure that sql is executed immediately,
         # not batched. Batching causes now() to be the same instant for
         # all items, which causes big problems.
         sesh.commit()
+        if print_:
+            print("\n------")
+            print(s)
+            print("\nmetadata_hx")
+            for row in sesh.execute(f"SELECT * FROM {metadata_name}_hx"):
+                print(row)
+            print("\nmetadata")
+            for row in sesh.execute(f"SELECT * FROM {metadata_name}"):
+                print(row)
         return result
 
     execute(f"SET search_path TO {get_schema_name()}, public")
@@ -55,65 +66,10 @@ def test_update_metadata(
 
     # Perform the operations
     for op in operations:
-        execute(op)
+        execute(op, print_=print_info)
 
     # Check the final state of the metadata view
     metadata_col_names = ", ".join(name for name, _ in metadata_cols)
     result = execute(f"SELECT {metadata_col_names} FROM {metadata_name}")
     for expected, actual in zip(final_metadata, result):
-        assert actual == tuple(actual)
-
-
-@pytest.mark.usefixtures("new_db_left")
-def test_print(sesh_in_prepared_schema_left, setup_metadata_objects):
-    print("CURRENT HEAD", pycds.alembic.info.get_current_head())
-    sesh = sesh_in_prepared_schema_left
-
-    def execute(s):
-        result = sesh.execute(text(s))
-        # Commit is required to ensure that sql is executed immediately,
-        # not batched. Batching causes now() to be the same instant for
-        # all items, which causes big problems for the view.
-        sesh.commit()
-        return result
-
-    execute(f"SET search_path TO {get_schema_name()}, public")
-
-    # Set up sequence, view and table
-    setup_metadata_objects(
-        sesh,
-        "metadata",
-        "metadata_id",
-        [
-            ["item1", "text"],
-            ["item2", "text"],
-        ],
-    )
-
-    def exec_and_check(s):
-        print("\n------")
-        print(s)
-        execute(s)
-        print("\nmetadata_hx")
-        for row in execute("SELECT * FROM metadata_hx"):
-            print(row)
-        print("\nmetadata")
-        for row in execute("SELECT * FROM metadata"):
-            print(row)
-
-    exec_and_check("INSERT INTO metadata(item1, item2) VALUES ('A', 'B')")
-    exec_and_check("INSERT INTO metadata(item1, item2) VALUES ('C', 'D'), ('E', 'F')")
-    exec_and_check(
-        """
-        UPDATE metadata SET item1 = 'X' WHERE metadata_id = 3;
-        UPDATE metadata SET item2 = 'Y' WHERE metadata_id = 1;
-    """
-    )
-    exec_and_check("UPDATE metadata SET item2 = 'Q' WHERE item1 <> 'X'")
-    exec_and_check("DELETE FROM metadata WHERE item1 = 'X'")
-    exec_and_check("INSERT INTO metadata(item1, item2) VALUES ('L', 'M')")
-
-    # metadata2_id integer NOT NULL,
-    # gronk int,
-    # bargle timestamp,
-    # argle text
+        assert expected == tuple(actual)
