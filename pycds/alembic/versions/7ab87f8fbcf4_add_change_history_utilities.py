f2c138ec824d12fc245038b4f5dd446e24c0d8e7
rod-glover <rod.glover.ca@gmail.com>
2024-12-10 15:45:55 -0800

    Add migration skeleton

---
+++ pycds/alembic/versions/7ab87f8fbcf4_add_change_history_utilities.py
@@ -0,0 +1,24 @@
+"""Add change history utilities
+
+Revision ID: 7ab87f8fbcf4
+Revises: 081f17262852
+Create Date: 2024-12-10 15:45:19.379892
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "7ab87f8fbcf4"
+down_revision = "081f17262852"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
