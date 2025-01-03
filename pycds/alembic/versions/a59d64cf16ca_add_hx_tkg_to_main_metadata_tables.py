15fa1b46f1d14bfd6ae6610c1ebb6c40ae42ca45
rod-glover <rod.glover.ca@gmail.com>
2025-01-02 16:51:19 -0800

    Fix migration script and supporting utils

--- pycds/alembic/versions/a59d64cf16ca_add_hx_tkg_to_main_metadata_tables.py
+++ pycds/alembic/versions/a59d64cf16ca_add_hx_tkg_to_main_metadata_tables.py
@@ -5,15 +5,18 @@ Revises: 7ab87f8fbcf4
 Create Date: 2024-12-16 14:54:41.332001
 
 """
+from alembic import op
+
 from pycds import get_schema_name
 from pycds.alembic.change_history_utils import (
     add_history_cols_to_primary,
     create_history_table,
     populate_history_table,
-    create_history_triggers,
     drop_history_triggers,
     drop_history_table,
     drop_history_cols_from_primary,
+    create_history_table_triggers,
+    create_primary_table_triggers,
 )
 
 # revision identifiers, used by Alembic.
@@ -27,20 +30,25 @@ schema_name = get_schema_name()
 
 
 table_info = (
-    ("meta_network", "network_id", []),
-    # ("meta_station", "station_id", [("meta_network", "network_id")]),
-    # ("meta_history", "", [("meta_station", "station_id")]),
-    # ("meta_vars", "", [("meta_network", "network_id")]),
+    ("meta_network", "network_id", None),
+    ("meta_station", "station_id", [("meta_network", "network_id")]),
+    ("meta_history", "history_id", [("meta_station", "station_id")]),
+    ("meta_vars", "vars_id", [("meta_network", "network_id")]),
 )
 
 
 def upgrade():
-    print("Upgrading in schema", schema_name)
-    for table_name, primary_id_name, foreign_keys in table_info:
+    # We have to set the search_path so that the trigger functions fired when
+    # the history table is populated can find the functions that they call.
+    op.get_bind().execute(f"SET search_path TO {schema_name}, public")
+    for table_name, primary_key_name, foreign_keys in table_info:
         add_history_cols_to_primary(table_name)
-        create_history_table(table_name, primary_id_name)
-        populate_history_table(table_name, primary_id_name)
-        create_history_triggers(table_name, foreign_keys)
+        create_primary_table_triggers(table_name)
+
+        create_history_table(table_name, foreign_keys)
+        # History table triggers must be created before the table is populated.
+        create_history_table_triggers(table_name, foreign_keys)
+        populate_history_table(table_name, primary_key_name)
 
 
 def downgrade():
