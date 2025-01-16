fe8f34858cf16df561136a290bbd7a5a946a8bbb
rod-glover <rod.glover.ca@gmail.com>
2025-01-16 13:21:55 -0800

    Fix revision checking

--- tests/alembic_migrations/test_check_migration_version.py
+++ tests/alembic_migrations/test_check_migration_version.py
@@ -8,7 +8,7 @@ from pycds.database import check_migration_version
 
 @pytest.mark.update20
 def test_get_current_head():
-    assert get_current_head() == "8c05da87cb79"
+    assert get_current_head() == "758be4f4ce0f"
 
 
 @pytest.mark.update20
